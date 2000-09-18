bcdcccbcd86efd0d80e4a2cd31f12019d9cc97f3
hship <dev-null@apache.org>
2000-09-18 17:35:10 +0000

    Started ChangeLog (some time after release 0.1.1). [114704] Should allow source tree to live in different directory [114708] JBE should remove duplicates from the classpath
    git-svn-id: https://svn.eu.apache.org/repos/asf/jakarta/tapestry/trunk@241910 13f79535-47bb-0310-9956-ffa450edef68

--- JBE/Package.mk
+++ JBE/Package.mk
@@ -27,21 +27,7 @@
 
 include $(SYS_MAKEFILE_DIR)/CommonDefs.mk
 
-default: remake-check
-	@$(REMAKE_MODULE) default
-
-# File which records the relative path to the Module
-
-MOD_DIR_FILE := $(SYS_BUILD_DIR_NAME)/mod_dir
-
-# Used to re-invoke make in the Module.  This allows local targets
-# such as compile to work, even though these require that the Jar
-# module makefile invoke the make in the Package.
-
-REMAKE_MODULE = \
-	$(MAKE) --unix -C ${shell $(CAT) $(MOD_DIR_FILE)}
-
-catalog: initialize catalog-java catalog-resources catalog-rmi
+catalog: catalog-java catalog-resources catalog-rmi
 
 ifneq "$(JAVA_FILES)" ""
 
@@ -61,50 +47,16 @@ catalog-resources: $(wildcard $(RESOURCE_FILES))
 	
 endif
 
+# RMI_CLASSES is a list of class names (not file names), so it can't
+# be wildcarded.
+
 ifneq "$(RMI_CLASSES)" ""
 
 catalog-rmi:
-	@$(ECHO) "$(addprefix $(PACKAGE)$(DOT),$(RMI_CLASSES))" >> $(MOD_RMI_CLASS_CATALOG)
+	@$(ECHO) "$(addprefix $(PACKAGE)$(DOT),$(RMI_CLASSES))" \
+		>> $(MOD_RMI_CLASS_CATALOG)
 
 endif
 
-
-# Initialize is responsible for saving MOD_DIR (relative path from
-# this package to the root module directory) and PACKAGE
-# (the Java package name for this package).  This is later used
-# by REMAKE_PACKAGE to re-invoke the module level make for this
-# package.
-
-initialize:
-	@if [ ! -r $(MOD_DIR_FILE) ] ; \
-	then \
-		$(MKDIRS) $(SYS_BUILD_DIR_NAME) ; \
-		$(ECHO) $(MOD_DIR) > $(MOD_DIR_FILE) ; \
-	fi
-
-# MOD_DIR not defined; so make was invoked for just this directory.
-# Invoke Jar make for just this package.
-
-remake-check:
-	@if [ ! -r $(MOD_DIR_FILE) ] ; \
-	then \
-	  $(ECHO) "Package has been cleaned. " \
-		"You must re-invoke make at the top level." ; \
-	  exit 1; \
-	fi
-
-compile: remake-check
-	@$(REMAKE_MODULE) compile-package
-
-force: remake-check
-	#$(REMAKE_MODULE) force
-	
-install: remake-check
-	@$(REMAKE_MODULE) install	
-
-clean:
-	@$(RMDIRS) $(SYS_BUILD_DIR_NAME)
-
-.PHONY: default compile clean install force
 .PHONY: catalog catalog-java catalog-rmi catalog-resources
 
