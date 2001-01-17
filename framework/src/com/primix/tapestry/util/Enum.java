596253c3bbf076fa6979bb9248b8bbf832d65d6f
hship <dev-null@apache.org>
2001-01-17 19:31:15 +0000

    Tiny efficiency improvement in Enum serialization.
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/tapestry/trunk@242071 13f79535-47bb-0310-9956-ffa450edef68

--- framework/src/com/primix/tapestry/util/Enum.java
+++ framework/src/com/primix/tapestry/util/Enum.java
@@ -59,12 +59,12 @@ import java.util.*;
  
 public class Enum implements Serializable
 {
-    private String enumerationId;
+	private transient String enumerationId;
 
 	/**
 	 *  Used to resolve tokens back to <code>Enum</code> instances during deserialization.
 	 *  The key is a {@link EnumToken} that identifies the class of the the Enum and
-     *  its serialization id.
+	*  its serialization id.
 	 *
 	 */
 
@@ -78,53 +78,53 @@ public class Enum implements Serializable
 
 	public String getEnumerationId()
 	{
-    	return enumerationId;
+		return enumerationId;
 	}
 
 	/**
-     *  Registers the new Enum.  The serializationId must be non-null and unique
-     *  among all instances of Enum for the same class.
-     *
-     */
-     
+	*  Registers the new Enum.  The serializationId must be non-null and unique
+	*  among all instances of Enum for the same class.
+	*
+	*/
+
 	protected Enum(String enumerationId)
 	{
-    	if (enumerationId == null)
-        	throw new RuntimeException("Must provide non-null enumerationId.");
-        
-        this.enumerationId = enumerationId;
-        
-        register(this);
+		if (enumerationId == null)
+			throw new RuntimeException("Must provide non-null enumerationId.");
+
+		this.enumerationId = enumerationId;
+
+		register(this);
 	}
-            
+
 	/**
-     *  Returns the class name (with the package stripped off), and the serializationId,
-     *  seperated by a period.
-     *  For example, "Suit.CLUBS" for org.example.Suit with a serializationId of "CLUBS".
-     *
-     */
-     
-    public String toString()
-    {
-    	StringBuffer buffer;
-        String name;
-        int dot;
-        
-        name = getClass().getName();
-        
-        dot = name.lastIndexOf('.');
- 		if (dot > 0)
-          name = name.substring(dot + 1);
-                
- 		buffer = new StringBuffer();
-        buffer.append(name);
-        buffer.append('[');
-        buffer.append(enumerationId);
-        buffer.append(']');
-        
-        return buffer.toString();
-    }
-        
+	*  Returns the class name (with the package stripped off), and the serializationId,
+	*  seperated by a period.
+	*  For example, "Suit.CLUBS" for org.example.Suit with a serializationId of "CLUBS".
+	*
+	*/
+
+	public String toString()
+	{
+		StringBuffer buffer;
+		String name;
+		int dot;
+
+		name = getClass().getName();
+
+		dot = name.lastIndexOf('.');
+		if (dot > 0)
+			name = name.substring(dot + 1);
+
+		buffer = new StringBuffer();
+		buffer.append(name);
+		buffer.append('[');
+		buffer.append(enumerationId);
+		buffer.append(']');
+
+		return buffer.toString();
+	}
+
 	/**
 	*  Registers the singleton instance for later re-use during deserialization.
 	*
@@ -132,25 +132,25 @@ public class Enum implements Serializable
 
 	private static synchronized void register(Enum enum)
 	{
-    	EnumToken key;
-        
-        key = new EnumToken(enum);
-        
-        if (identity.containsKey(key))
-        	throw new RuntimeException(
-            "com.primix.foundation.Enum.register(): " +
-            key + " is already registered.  " +
-            "The enumerationId property must be unique within the class.");
-            
+		EnumToken key;
+
+		key = new EnumToken(enum);
+
+		if (identity.containsKey(key))
+			throw new RuntimeException(
+				"com.primix.foundation.Enum.register(): " +
+				key + " is already registered.  " +
+				"The enumerationId property must be unique within the class.");
+
 		identity.put(new EnumToken(enum), enum);
 	}
 
-    /**
-     *  Invoked from subclass' <code>readResolve()</code> method.
-     *
-     *  <p>This is a bit of serialization black magic ... the method
-     *  <code>readResolve()</code> MUST BE implemented by the actual class, it can't
-     *  be inherited.  Subclasses should simply invoke this method:
+	/**
+	*  Invoked from subclass' <code>readResolve()</code> method.
+	*
+	*  <p>This is a bit of serialization black magic ... the method
+	*  <code>readResolve()</code> MUST BE implemented by the actual class, it can't
+	*  be inherited.  Subclasses should simply invoke this method:
 	 *
 	 *  <p><pre>
 	 *  private Object readResolve()
@@ -162,30 +162,53 @@ public class Enum implements Serializable
 	 * <p>Since
 	 *  <code>readResolve()</code> returns a <code>java.lang.Object</code>
 	 *  there's no reason to cast the singleton to <code>Enum</code>.
-     *
-     */
-    
+	*
+	*/
+
 	protected Object getSingleton()
 	{
-    	Object result;
-        EnumToken key;
-        
-    	key = new EnumToken(this);
-        
-        // Get the previously registered version.
-        
-        synchronized(identity)     
-        {
-        	result = identity.get(key);
-        }
-        
-        if (result == null)
-        	throw new RuntimeException(
-            key + " does not map to a known instance in this JVM.  " +
-            "This instance of " + getClass().getName() + " was serialized in a JVM that defined " +
-            "a different set of values.");
-            
-        return result;
+		Object result;
+		EnumToken key;
+
+		key = new EnumToken(this);
+
+		// Get the previously registered version.
+
+		synchronized(identity)
+		{
+			result = identity.get(key);
+		}
+
+		if (result == null)
+			throw new RuntimeException(
+				key + " does not map to a known instance in this JVM.  " +
+				"This instance of " + getClass().getName() + " was serialized in a JVM that defined " +
+				"a different set of values.");
+
+		return result;
+	}
+
+	/**
+	 *  Writes the enumerationId (which is marked transient) to the output stream,
+	 *  using {@link ObjectOutputStream#writeUTF(String)}, which is more efficient.
+	 *
+	 */
+
+	private void writeObject(ObjectOutputStream out)
+	throws IOException
+	{
+		out.writeUTF(enumerationId);
+	}
+
+	/**
+	  *  Reads the state written by {@link #writeObject(ObjectOutputStream)}.
+	  *
+	  */
+
+	private void readObject(ObjectInputStream in)
+	throws IOException, ClassNotFoundException
+	{
+		enumerationId = in.readUTF();
 	}
 
 
