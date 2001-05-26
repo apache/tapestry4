eab56c6a7f0372305345074ce7ea0d4fa706d5f1
hship <dev-null@apache.org>
2001-05-26 14:07:48 +0000

    Add a warning to the InsertBlock javadoc about lifecycle methods.
    git-svn-id: https://svn.eu.apache.org/repos/asf/jakarta/tapestry/trunk@242190 13f79535-47bb-0310-9956-ffa450edef68

--- framework/src/com/primix/tapestry/components/InsertBlock.java
+++ framework/src/com/primix/tapestry/components/InsertBlock.java
@@ -7,9 +7,9 @@
  * Watertown, MA 02472
  * http://www.primix.com
  * mailto:hship@primix.com
- * 
+ *
  * This library is free software.
- * 
+ *
  * You may redistribute it and/or modify it under the terms of the GNU
  * Lesser General Public License as published by the Free Software Foundation.
  *
@@ -33,12 +33,25 @@ import com.primix.tapestry.*;
 /**
  *  Renders the text and components wrapped by a {@link Block} component.
  *
+ *  <p>It is possible for an InsertBlock to obtain a Block
+ *  from a page <em>other than</em> the render page.  This works, even when
+ *  the Block contains links, forms and form components.  The action and
+ *  direct services will create URLs that properly address this situation.
+ *
+ *  <p>However, because the rendering page can't know about these foriegn
+ *  Blocks, it can't run lifecycle methods on components from them.  Again,
+ *  {@link ILifecycle} methods for components dragged in from other pages
+ *  via InsertBlock will <em>not</em> be executed.  This specifically
+ *  affects the methods {@link ILifecycle#prepareForRender(IRequestCycle)}
+ *  and {@link ILifecycle#cleanupAfterRender(IRequestCycle)}.
+ *
+ * <p>
  * <table border=1>
- * <tr> 
+ * <tr>
  *    <th>Parameter</th>
  *    <th>Type</th>
- *	  <th>Read / Write </th>
- *    <th>Required</th> 
+ *    <th>Read / Write </th>
+ *    <th>Required</th>
  *    <th>Default</th>
  *    <th>Description</th>
  * </tr>
@@ -62,35 +75,35 @@ import com.primix.tapestry.*;
 
 public class InsertBlock extends AbstractComponent
 {
-	private IBinding blockBinding;
-	
-	public void setBlockBinding(IBinding value)
-	{
-		blockBinding = value;
-	}
-	
-	public IBinding getBlockBinding()
-	{
-		return blockBinding;
-	}
-	
-	/**
-	 *  If the block parameter is bound and not null,
-	 *  then {@link IComponent#renderWrapped(IResponseWriter, IRequestCycle)}
-	 *  is invoked on it.
-	 *
-	 */
-	 
-	public void render(IResponseWriter writer, IRequestCycle cycle)
-	throws RequestCycleException
-	{
-		if (blockBinding == null)
-			return;
-		
-		Block block = (Block)blockBinding.getObject("block", Block.class);
-		
-		if (block != null)
-			block.renderWrapped(writer, cycle);	
-	}
+    private IBinding blockBinding;
+    
+    public void setBlockBinding(IBinding value)
+    {
+        blockBinding = value;
+    }
+    
+    public IBinding getBlockBinding()
+    {
+        return blockBinding;
+    }
+    
+    /**
+     *  If the block parameter is bound and not null,
+     *  then {@link IComponent#renderWrapped(IResponseWriter, IRequestCycle)}
+     *  is invoked on it.
+     *
+     */
+     
+    public void render(IResponseWriter writer, IRequestCycle cycle)
+    throws RequestCycleException
+    {
+        if (blockBinding == null)
+            return;
+        
+        Block block = (Block)blockBinding.getObject("block", Block.class);
+        
+        if (block != null)
+            block.renderWrapped(writer, cycle);
+    }
 }
 
