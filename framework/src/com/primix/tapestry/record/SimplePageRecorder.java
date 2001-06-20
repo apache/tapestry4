dba36d01009118af6aa8a3ec844e0ee09c64c00c
hship <dev-null@apache.org>
2001-06-20 18:43:10 +0000

    [434855] SimplePageRecorder failure w/ EJBs
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/tapestry/trunk@242223 13f79535-47bb-0310-9956-ffa450edef68

--- framework/src/com/primix/tapestry/record/SimplePageRecorder.java
+++ framework/src/com/primix/tapestry/record/SimplePageRecorder.java
@@ -51,39 +51,39 @@ import com.primix.tapestry.engine.*;
  */
 
 public class SimplePageRecorder 
-extends PageRecorder
-implements Externalizable
+	extends PageRecorder
+	implements Externalizable
 {
 	private final static int MAP_SIZE = 7;
-
+	
 	/**
-	*  Dictionary of changes, keyed on an instance of ChangeKey
-	*  (which enapsulates component path and property name).  The
-	*  value is the new value for the object.
-	*
-	*/
-
+	 *  Dictionary of changes, keyed on an instance of ChangeKey
+	 *  (which enapsulates component path and property name).  The
+	 *  value is the new value for the object.
+	 *
+	 */
+	
 	private Map changes;
-
+	
 	/**
-	*  Simply clears the dirty flag, because there is no external place
-	*  to store changed page properties.  Sets the locked flag to prevent
-    * subsequent changes from occuring now.
-	*
-	*/
-
+	 *  Simply clears the dirty flag, because there is no external place
+	 *  to store changed page properties.  Sets the locked flag to prevent
+	 * subsequent changes from occuring now.
+	 *
+	 */
+	
 	public void commit()
-	throws PageRecorderCommitException
+		throws PageRecorderCommitException
 	{
 		dirty = false;
-        locked = true;
+		locked = true;
 	}
-
+	
 	/**
 	 *  Returns true if the recorder has any changes recorded.
 	 *
 	 */
-	 
+	
 	public boolean getHasChanges()
 	{
 		if (changes == null)
@@ -91,143 +91,150 @@ implements Externalizable
 		
 		return (changes.size() > 0);	
 	}
-
+	
 	public Collection getChanges()
 	{
-    	Collection result;
-        Iterator i;
-        Map.Entry entry;
-        PageChange change;
-        int count;
-        ChangeKey key;
-        Object value;
-        
+		Collection result;
+		Iterator i;
+		Map.Entry entry;
+		PageChange change;
+		int count;
+		ChangeKey key;
+		Object value;
+		
 		if (changes == null)
 			return Collections.EMPTY_LIST;
-
+		
 		count = changes.size();
-        result = new ArrayList(count);
-        
-        i = changes.entrySet().iterator();
-        while (i.hasNext())
-        {
-        	entry = (Map.Entry)i.next();
-            
-            key = (ChangeKey)entry.getKey();
-            
-            value = entry.getValue();
-            
-            change = new PageChange(key.componentPath, key.propertyName, value);
-            
-            result.add(change);
-        }
-
+		result = new ArrayList(count);
+		
+		i = changes.entrySet().iterator();
+		while (i.hasNext())
+		{
+			entry = (Map.Entry)i.next();
+			
+			key = (ChangeKey)entry.getKey();
+			
+			value = entry.getValue();
+			
+			change = new PageChange(key.componentPath, key.propertyName, value);
+			
+			result.add(change);
+		}
+		
 		return result;
 	}
-
+	
 	protected void recordChange(String componentPath, String propertyName, Object newValue)
 	{
 		ChangeKey key;
 		Object oldValue;
-
+		
 		key = new ChangeKey(componentPath, propertyName);
-
+		
 		if (changes == null)
 			changes = new HashMap(MAP_SIZE);
-
+		
 		// Check the prior value.  If this is not an actual change,
 		// then don't bother recording it, or marking this page recorder
 		// dirty.
-
+		
 		oldValue = changes.get(key);
 		if (newValue == oldValue)
 			return;
-
-		if (oldValue != null &&
-			oldValue.equals(newValue))
-			return;
-
+		
+		try
+		{
+			if (oldValue != null &&
+					oldValue.equals(newValue))
+				return;
+		}
+		catch (Exception ex)
+		{
+			// Ignore.
+		}
+		
 		dirty = true;
-
+		
 		changes.put(key, newValue);
 	}
-
+	
     /**
-     *  Reads the state stored by {@link #writeExternal(ObjectOutput)}.
-     *
-     */
-
+	 *  Reads the state stored by {@link #writeExternal(ObjectOutput)}.
+	 *
+	 */
+	
     public void readExternal(ObjectInput in)
-    throws IOException, ClassNotFoundException
+		throws IOException, ClassNotFoundException
     {
-        String componentPath;
-        int count = in.readInt();
-
-        if (count == 0)
-            return;
-
-        changes = new HashMap(MAP_SIZE);
-
-        for (int i = 0; i < count; i++)
-        {
-            // Read a boolean ... if true, a UTF string follows.
-
-            if (in.readBoolean())
-                componentPath = (String)in.readObject();
-            else
-                componentPath = null;
-
-            String propertyName = in.readUTF();
-            Object rawValue = in.readObject();
+		String componentPath;
+		int count = in.readInt();
+		
+		if (count == 0)
+			return;
+		
+		changes = new HashMap(MAP_SIZE);
+		
+		for (int i = 0; i < count; i++)
+		{
+			// Read a boolean ... if true, a UTF string follows.
+			
+			if (in.readBoolean())
+				componentPath = (String)in.readObject();
+			else
+				componentPath = null;
+			
+			String propertyName = in.readUTF();
+			Object rawValue = in.readObject();
 			Object value = restoreValue(rawValue);
-
-            changes.put(new ChangeKey(componentPath, propertyName), value);
-        }
-
+			
+			changes.put(new ChangeKey(componentPath, propertyName), value);
+		}
+		
     }
-
+	
     /**
-     *  Writes the changes.  Writes the number of changes as an int, which
-     *  may be zero.
-     *
-     *  <p>For each change, writes:
-     *  <ul>
-     *  <li>componentPath ({@link String}, may be null)
-     *  <li>propertyName ({@link String})
-     *  <li>value (may be null)
-     *  </ul>
-     *
-     */
-
+	 *  Writes the changes.  Writes the number of changes as an int, which
+	 *  may be zero.
+	 *
+	 *  <p>For each change, writes:
+	 *  <ul>
+	 *  <li>componentPath ({@link String}, may be null)
+	 *  <li>propertyName ({@link String})
+	 *  <li>value (may be null)
+	 *  </ul>
+	 *
+	 */
+	
     public void writeExternal(ObjectOutput out)
-    throws IOException
+		throws IOException
     {
-        if (changes == null)
-        {
-            out.writeInt(0);
-            return;
-        }
-
-        out.writeInt(changes.size());
-        Iterator i = changes.entrySet().iterator();
-
-        while (i.hasNext())
-        {
-            Map.Entry entry = (Map.Entry)i.next();
-            ChangeKey key = (ChangeKey)entry.getKey();
-
-            // To avoid writeObject(), we write a boolean
-            // indicating whether there's a non-null UTF string
-            // following.
-
-            out.writeBoolean(key.componentPath != null);
-
-            if (key.componentPath != null)
-                out.writeObject(key.componentPath);
-
-            out.writeUTF(key.propertyName);
-            out.writeObject(persistValue(entry.getValue()));
-        }
+		if (changes == null)
+		{
+			out.writeInt(0);
+			return;
+		}
+		
+		out.writeInt(changes.size());
+		Iterator i = changes.entrySet().iterator();
+		
+		while (i.hasNext())
+		{
+			Map.Entry entry = (Map.Entry)i.next();
+			ChangeKey key = (ChangeKey)entry.getKey();
+			
+			// To avoid writeObject(), we write a boolean
+			// indicating whether there's a non-null UTF string
+			// following.
+			
+			out.writeBoolean(key.componentPath != null);
+			
+			if (key.componentPath != null)
+				out.writeObject(key.componentPath);
+			
+			out.writeUTF(key.propertyName);
+			out.writeObject(persistValue(entry.getValue()));
+		}
     }
 }
 
