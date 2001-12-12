165eb05d80d6ce2bff6c389e246ed3247065b9ed
hship <dev-null@apache.org>
2001-12-12 12:45:16 +0000

    update framework version to 1.0.8
    git-svn-id: https://svn.eu.apache.org/repos/asf/jakarta/tapestry/trunk@242338 13f79535-47bb-0310-9956-ffa450edef68

--- framework/src/com/primix/tapestry/Tapestry.java
+++ framework/src/com/primix/tapestry/Tapestry.java
@@ -55,7 +55,7 @@ public final class Tapestry
 	 *
 	 */
 
-	public static final String VERSION = "1.0.7";
+	public static final String VERSION = "1.0.8";
 
 	/**
 	 *  A {@link Map} that links Locale names (as in {@link Locale#toString()} to
