371ba0957a2e76152b775fc44ff05ae2c87b9b76
hship <dev-null@apache.org>
2002-02-09 15:20:53 +0000

    Update tests for JUnit 3.7, make it easy to run JUnit GUI
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/tapestry/trunk@242440 13f79535-47bb-0310-9956-ffa450edef68

--- junit/src/net/sf/tapestry/junit/valid/TestValidationDelegate.java
+++ junit/src/net/sf/tapestry/junit/valid/TestValidationDelegate.java
@@ -23,17 +23,18 @@
  * Lesser General Public License for more details.
  *
  */
- 
+
 package net.sf.tapestry.junit.valid;
 
+import com.primix.tapestry.IRender;
 import com.primix.tapestry.valid.IField;
 import com.primix.tapestry.valid.IFieldTracking;
+import com.primix.tapestry.valid.RenderString;
 import com.primix.tapestry.valid.ValidationConstraint;
 import com.primix.tapestry.valid.ValidationDelegate;
 import com.primix.tapestry.valid.ValidatorException;
 import java.util.List;
 import junit.framework.TestCase;
-
 
 /**
  *  Test the class {@link ValidationDelegate}. 
@@ -48,127 +49,135 @@ import junit.framework.TestCase;
 public class TestValidationDelegate extends TestCase
 {
 	private ValidationDelegate d = new ValidationDelegate();
-	
+
 	public TestValidationDelegate(String name)
 	{
 		super(name);
 	}
-	
+
 	public void testHasErrorsEmpty()
 	{
 		assertEquals(false, d.getHasErrors());
 	}
-	
+
 	public void testFirstErrorEmpty()
 	{
 		assertNull(d.getFirstError());
 	}
-	
+
 	public void testInvalidInput()
 	{
 		IField f = new TestingField("testAdd");
 		String errorMessage = "Need a bigger one.";
-		
-		d.setField(f);
-		d.record(new ValidatorException(errorMessage, 
-		ValidationConstraint.TOO_LARGE, "Bad Stuff"));
-		
+
+		d.setFormComponent(f);
+		d.record(
+			new ValidatorException(
+				errorMessage,
+				ValidationConstraint.TOO_LARGE,
+				"Bad Stuff"));
+
 		List fieldTracking = d.getFieldTracking();
-		
+
 		assertEquals(1, fieldTracking.size());
-		
-		IFieldTracking t = (IFieldTracking)fieldTracking.get(0);
-		
-		assertEquals(f, t.getField());
-		assertEquals(errorMessage, t.getErrorMessage());
+
+		IFieldTracking t = (IFieldTracking) fieldTracking.get(0);
+
+		assertEquals(f, t.getFormComponent());
+		checkRender(errorMessage, t);
 		assertEquals("testAdd", t.getFieldName());
 		assertEquals("Bad Stuff", t.getInvalidInput());
 		assertEquals(ValidationConstraint.TOO_LARGE, t.getConstraint());
-		
+
 		assertTrue(d.getHasErrors());
-		assertEquals(errorMessage, d.getFirstError());
+		assertEquals(errorMessage, ((RenderString)(d.getFirstError())).getString());
 	}
-	
+
+	private void checkRender(String errorMessage, IFieldTracking tracking)
+	{
+		IRender render = tracking.getRenderer();
+
+		assertEquals(errorMessage, ((RenderString) render).getString());
+	}
+
 	public void testMultipleInvalidInput()
 	{
 		IField f1 = new TestingField("input1");
 		String e1 = "And now for something completely different.";
-		IField f2 = new TestingField("input2");	
+		IField f2 = new TestingField("input2");
 		String e2 = "A man with three buttocks.";
-		
-		d.setField(f1);
+
+		d.setFormComponent(f1);
 		d.record(new ValidatorException(e1, null, "Monty"));
-		
-		d.setField(f2);
+
+		d.setFormComponent(f2);
 		d.record(new ValidatorException(e2, null, "Python"));
-		
+
 		List fieldTracking = d.getFieldTracking();
 		assertEquals(2, fieldTracking.size());
-		
-		IFieldTracking t = (IFieldTracking)fieldTracking.get(0);
-		
-		assertEquals(f1, t.getField());
-		assertEquals(e1, t.getErrorMessage());
-		
-		t =  (IFieldTracking)fieldTracking.get(1);
+
+		IFieldTracking t = (IFieldTracking) fieldTracking.get(0);
+
+		assertEquals(f1, t.getFormComponent());
+		checkRender(e1, t);
+
+		t = (IFieldTracking) fieldTracking.get(1);
 		assertEquals("Python", t.getInvalidInput());
-		assertEquals(e2, t.getErrorMessage());
-		assertEquals(f2, t.getField());
+		checkRender(e2, t);
+		assertEquals(f2, t.getFormComponent());
 	}
-	
+
 	public void testReset()
 	{
 		IField f1 = new TestingField("input1");
 		String e1 = "And now for something completely different.";
-		IField f2 = new TestingField("input2");	
+		IField f2 = new TestingField("input2");
 		String e2 = "A man with three buttocks.";
-		
-		d.setField(f1);
+
+		d.setFormComponent(f1);
 		d.record(new ValidatorException(e1, null, "Monty"));
-		
-		d.setField(f2);
+
+		d.setFormComponent(f2);
 		d.record(new ValidatorException(e2, null, "Python"));
-		
+
 		// Now, wipe out info on f1
-		
-		d.setField(f1);
+
+		d.setFormComponent(f1);
 		d.reset();
-		
+
 		List fieldTracking = d.getFieldTracking();
 		assertEquals(1, fieldTracking.size());
-		
-		
-		IFieldTracking t =  (IFieldTracking)fieldTracking.get(0);
+
+		IFieldTracking t = (IFieldTracking) fieldTracking.get(0);
 		assertEquals("Python", t.getInvalidInput());
-		assertEquals(e2, t.getErrorMessage());
-		assertEquals(f2, t.getField());
+		checkRender(e2, t);
+		assertEquals(f2, t.getFormComponent());
 	}
 
 	public void testResetAll()
 	{
 		IField f1 = new TestingField("input1");
 		String e1 = "And now for something completely different.";
-		IField f2 = new TestingField("input2");	
+		IField f2 = new TestingField("input2");
 		String e2 = "A man with three buttocks.";
-		
-		d.setField(f1);
+
+		d.setFormComponent(f1);
 		d.record(new ValidatorException(e1, null, "Monty"));
-		
-		d.setField(f2);
-		d.record(new ValidatorException(e2, null, "Python"));;
-		
-		d.setField(f1);
+
+		d.setFormComponent(f2);
+		d.record(new ValidatorException(e2, null, "Python"));
+		;
+
+		d.setFormComponent(f1);
 		d.reset();
-		
-		d.setField(f2);
+
+		d.setFormComponent(f2);
 		d.reset();
-		
-		
+
 		List fieldTracking = d.getFieldTracking();
 		assertEquals(0, fieldTracking.size());
-		
+
 		assertEquals(false, d.getHasErrors());
-		assertNull(d.getFirstError());		
+		assertNull(d.getFirstError());
 	}
 }
-
