b161908bde38de3b505b8fc260b3bcdb9b788767
hship <dev-null@apache.org>
2002-05-02 21:52:19 +0000

    [ 549479 ] Rename packages to net.sf.tapestry
    git-svn-id: https://svn.eu.apache.org/repos/asf/jakarta/tapestry/trunk@242533 13f79535-47bb-0310-9956-ffa450edef68

--- framework/src/net/sf/tapestry/script/IScriptToken.java
+++ framework/src/net/sf/tapestry/script/IScriptToken.java
@@ -1,43 +1,42 @@
-/*
- * Tapestry Web Application Framework
- * Copyright (c) 2000-2001 by Howard Lewis Ship
- *
- * Howard Lewis Ship
- * http://sf.net/projects/tapestry
- * mailto:hship@users.sf.net
- *
- * This library is free software.
- *
- * You may redistribute it and/or modify it under the terms of the GNU
- * Lesser General Public License as published by the Free Software Foundation.
- *
- * Version 2.1 of the license should be included with this distribution in
- * the file LICENSE, as well as License.html. If the license is not
- * included with this distribution, you may find a copy at the FSF web
- * site at 'www.gnu.org' or 'www.fsf.org', or you may write to the
- * Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139 USA.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied waranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- */
+//
+// Tapestry Web Application Framework
+// Copyright (c) 2000-2002 by Howard Lewis Ship
+//
+// Howard Lewis Ship
+// http://sf.net/projects/tapestry
+// mailto:hship@users.sf.net
+//
+// This library is free software.
+//
+// You may redistribute it and/or modify it under the terms of the GNU
+// Lesser General Public License as published by the Free Software Foundation.
+//
+// Version 2.1 of the license should be included with this distribution in
+// the file LICENSE, as well as License.html. If the license is not
+// included with this distribution, you may find a copy at the FSF web
+// site at 'www.gnu.org' or 'www.fsf.org', or you may write to the
+// Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139 USA.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied waranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
 
 package net.sf.tapestry.script;
 
-import com.primix.tapestry.*;
-
-import net.sf.tapestry.*;
+import net.sf.tapestry.ScriptException;
+import net.sf.tapestry.ScriptSession;
 
 /**
  *  Defines the responsibilities of a template token used by a
  *  {@link ParsedScript} ... that is, how the token
  *  can assist in generating output.
  *
- *  @author Howard Ship
+ *  @author Howard Lewis Ship
  *  @version $Id$
- */
+ * 
+ **/
 
 public interface IScriptToken
 {
@@ -49,7 +48,7 @@ public interface IScriptToken
 	 *  <p>Top level tokens (such as BodyToken) can expect that
 	 *  buffer will be null.
 	 *
-	 */
+	 **/
 
 	public void write(StringBuffer buffer, ScriptSession session)
 		throws ScriptException;
@@ -59,7 +58,7 @@ public interface IScriptToken
 	 *  of this token.
 	 *
 	 *  @since 0.2.9
-	 */
+	 **/
 
 	public void addToken(IScriptToken token);
 }
--- framework/src/net/sf/tapestry/script/ParsedScript.java
+++ framework/src/net/sf/tapestry/script/ParsedScript.java
@@ -1,78 +1,81 @@
-/*
- * Tapestry Web Application Framework
- * Copyright (c) 2000-2001 by Howard Lewis Ship
- *
- * Howard Lewis Ship
- * http://sf.net/projects/tapestry
- * mailto:hship@users.sf.net
- *
- * This library is free software.
- *
- * You may redistribute it and/or modify it under the terms of the GNU
- * Lesser General Public License as published by the Free Software Foundation.
- *
- * Version 2.1 of the license should be included with this distribution in
- * the file LICENSE, as well as License.html. If the license is not
- * included with this distribution, you may find a copy at the FSF web
- * site at 'www.gnu.org' or 'www.fsf.org', or you may write to the
- * Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139 USA.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied waranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- */
- 
+//
+// Tapestry Web Application Framework
+// Copyright (c) 2000-2002 by Howard Lewis Ship
+//
+// Howard Lewis Ship
+// http://sf.net/projects/tapestry
+// mailto:hship@users.sf.net
+//
+// This library is free software.
+//
+// You may redistribute it and/or modify it under the terms of the GNU
+// Lesser General Public License as published by the Free Software Foundation.
+//
+// Version 2.1 of the license should be included with this distribution in
+// the file LICENSE, as well as License.html. If the license is not
+// included with this distribution, you may find a copy at the FSF web
+// site at 'www.gnu.org' or 'www.fsf.org', or you may write to the
+// Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139 USA.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied waranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+
 package net.sf.tapestry.script;
 
-import com.primix.tapestry.*;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 
-import net.sf.tapestry.*;
-
-import java.util.*;
+import net.sf.tapestry.IScript;
+import net.sf.tapestry.ScriptException;
+import net.sf.tapestry.ScriptSession;
 
 /**
  *  A top level container for a number of {@link IScriptToken script tokens}.
  *
- *  @author Howard Ship
+ *  @author Howard Lewis Ship
  *  @version $Id$
  *  @since 0.2.9
- */
+ * 
+ **/
 
 public class ParsedScript implements IScript
 {
-	private String scriptPath;
+    private String scriptPath;
 
-	public ParsedScript(String scriptPath)
-	{
-		this.scriptPath = scriptPath;
-	}
+    public ParsedScript(String scriptPath)
+    {
+        this.scriptPath = scriptPath;
+    }
 
-	public String getScriptPath()
-	{
-		return scriptPath;
-	}
+    public String getScriptPath()
+    {
+        return scriptPath;
+    }
 
-	private List tokens = new ArrayList();
+    private List tokens = new ArrayList();
 
-	public void addToken(IScriptToken token)
-	{
-		tokens.add(token);
-	}
+    public void addToken(IScriptToken token)
+    {
+        tokens.add(token);
+    }
 
-	public ScriptSession execute(Map symbols) throws ScriptException
-	{
-		ScriptSession result = new ScriptSession(scriptPath, symbols);
-		Iterator i = tokens.iterator();
+    public ScriptSession execute(Map symbols) throws ScriptException
+    {
+        ScriptSession result = new ScriptSession(scriptPath, symbols);
+        Iterator i = tokens.iterator();
 
-		while (i.hasNext())
-		{
-			IScriptToken token = (IScriptToken) i.next();
+        while (i.hasNext())
+        {
+            IScriptToken token = (IScriptToken) i.next();
 
-			token.write(null, result);
-		}
+            token.write(null, result);
+        }
 
-		return result;
-	}
+        return result;
+    }
 }
