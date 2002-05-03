80d12b5a0ce3f73dd926731024a65a3b9eee5e6f
hship <dev-null@apache.org>
2002-05-03 03:11:07 +0000

    [ 549479 ] Rename packages to net.sf.tapestry
    git-svn-id: https://svn.eu.apache.org/repos/asf/jakarta/tapestry/trunk@242534 13f79535-47bb-0310-9956-ffa450edef68

--- framework/src/net/sf/tapestry/spec/ContainedComponent.java
+++ framework/src/net/sf/tapestry/spec/ContainedComponent.java
@@ -1,28 +1,27 @@
-/*
- * Tapestry Web Application Framework
- * Copyright (c) 2000-2002 by Howard Lewis Ship
- *
- * Howard Lewis Ship
- * http://sf.net/projects/tapestry
- * mailto:hship@users.sf.net
- *
- * This library is free software.
- *
- * You may redistribute it and/or modify it under the terms of the GNU
- * Lesser General Public License as published by the Free Software Foundation.
- *
- * Version 2.1 of the license should be included with this distribution in
- * the file LICENSE, as well as License.html. If the license is not
- * included with this distribution, you may find a copy at the FSF web
- * site at 'www.gnu.org' or 'www.fsf.org', or you may write to the
- * Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139 USA.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied waranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- */
+//
+// Tapestry Web Application Framework
+// Copyright (c) 2000-2002 by Howard Lewis Ship
+//
+// Howard Lewis Ship
+// http://sf.net/projects/tapestry
+// mailto:hship@users.sf.net
+//
+// This library is free software.
+//
+// You may redistribute it and/or modify it under the terms of the GNU
+// Lesser General Public License as published by the Free Software Foundation.
+//
+// Version 2.1 of the license should be included with this distribution in
+// the file LICENSE, as well as License.html. If the license is not
+// included with this distribution, you may find a copy at the FSF web
+// site at 'www.gnu.org' or 'www.fsf.org', or you may write to the
+// Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139 USA.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied waranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
 
 package net.sf.tapestry.spec;
 
@@ -36,8 +35,9 @@ import java.util.Map;
  * get the contained component's specification, as well as any bindings
  * for the component.
  *
- * @author Howard Ship
+ * @author Howard Lewis Ship
  * @version $Id$
+ * 
  **/
 
 public class ContainedComponent
--- framework/src/net/sf/tapestry/util/Decorator.java
+++ framework/src/net/sf/tapestry/util/Decorator.java
@@ -1,28 +1,27 @@
-/*
- * Tapestry Web Application Framework
- * Copyright (c) 2000-2002 by Howard Lewis Ship
- *
- * Howard Lewis Ship
- * http://sf.net/projects/tapestry
- * mailto:hship@users.sf.net
- *
- * This library is free software.
- *
- * You may redistribute it and/or modify it under the terms of the GNU
- * Lesser General Public License as published by the Free Software Foundation.
- *
- * Version 2.1 of the license should be included with this distribution in
- * the file LICENSE, as well as License.html. If the license is not
- * included with this distribution, you may find a copy at the FSF web
- * site at 'www.gnu.org' or 'www.fsf.org', or you may write to the
- * Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139 USA.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied waranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- */
+//
+// Tapestry Web Application Framework
+// Copyright (c) 2000-2002 by Howard Lewis Ship
+//
+// Howard Lewis Ship
+// http://sf.net/projects/tapestry
+// mailto:hship@users.sf.net
+//
+// This library is free software.
+//
+// You may redistribute it and/or modify it under the terms of the GNU
+// Lesser General Public License as published by the Free Software Foundation.
+//
+// Version 2.1 of the license should be included with this distribution in
+// the file LICENSE, as well as License.html. If the license is not
+// included with this distribution, you may find a copy at the FSF web
+// site at 'www.gnu.org' or 'www.fsf.org', or you may write to the
+// Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139 USA.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied waranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
 
 package net.sf.tapestry.util;
 
@@ -33,11 +32,10 @@ import java.util.Map;
 
 import org.apache.log4j.Category;
 
-import net.sf.tapestry.*;
 import net.sf.tapestry.Tapestry;
 
 /**
- *  An implementation of the Decorator pattern.  The decorator
+ *  An implementation of the <b>Decorator</b> pattern.  The decorator
  *  pattern allows new functionality to be assigned to an existing class.
  *  As implemented here, the Decorator object is a smart lookup between
  *  a particular class (the class to be Decorated, called
@@ -75,247 +73,248 @@ import net.sf.tapestry.Tapestry;
  *
  *
  *  @version $Id$
- *  @author Howard Ship
+ *  @author Howard Lewis Ship
+ * 
  **/
 
 public class Decorator
 {
-	private static final Category CAT = Category.getInstance(Decorator.class);
+    private static final Category CAT = Category.getInstance(Decorator.class);
 
-	/**
-	 *  A Map of adaptor objects, keyed on registration Class.
-	 *
-	 **/
+    /**
+     *  A Map of adaptor objects, keyed on registration Class.
+     *
+     **/
 
-	private Map registrations = new HashMap();
+    private Map registrations = new HashMap();
 
-	/**
-	 *  A Map of adaptor objects, keyed on subject Class.
-	 *
-	 **/
+    /**
+     *  A Map of adaptor objects, keyed on subject Class.
+     *
+     **/
 
-	private Map cache = new HashMap();
+    private Map cache = new HashMap();
 
-	/**
-	 *  Registers an adaptor for a registration class.
-	 *
-	 *  @throws IllegalArgumentException if an adaptor has already
-	 *  been registered for the given class.
-	 **/
+    /**
+     *  Registers an adaptor for a registration class.
+     *
+     *  @throws IllegalArgumentException if an adaptor has already
+     *  been registered for the given class.
+     **/
 
-	public void register(Class registrationClass, Object adaptor)
-	{
-		synchronized (registrations)
-		{
-			if (registrations.containsKey(registrationClass))
-				throw new IllegalArgumentException(
-					Tapestry.getString("Decorator.duplicate-registration", registrationClass.getName()));
+    public void register(Class registrationClass, Object adaptor)
+    {
+        synchronized (registrations)
+        {
+            if (registrations.containsKey(registrationClass))
+                throw new IllegalArgumentException(
+                    Tapestry.getString("Decorator.duplicate-registration", registrationClass.getName()));
 
-			registrations.put(registrationClass, adaptor);
-		}
+            registrations.put(registrationClass, adaptor);
+        }
 
-			if (CAT.isInfoEnabled())
-				CAT.info("Registered " + adaptor + " for " + registrationClass.getName());
+        if (CAT.isInfoEnabled())
+            CAT.info("Registered " + adaptor + " for " + registrationClass.getName());
 
-		// Can't tell what is and isn't valid in the cache.
-		// Also, normally all registrations occur before any adaptors
-		// are searched for, so this is not a big deal.
+        // Can't tell what is and isn't valid in the cache.
+        // Also, normally all registrations occur before any adaptors
+        // are searched for, so this is not a big deal.
 
         synchronized (cache)
-	    {
-			cache.clear();
-	    }
-	}
+        {
+            cache.clear();
+        }
+    }
 
-	/**
-	 *  Gets the adaptor for the specified subjectClass.
-	 *
-	 *  @throws IllegalArgumentException if no adaptor could be found.
-	 *
-	 **/
+    /**
+     *  Gets the adaptor for the specified subjectClass.
+     *
+     *  @throws IllegalArgumentException if no adaptor could be found.
+     *
+     **/
 
-	public Object getAdaptor(Class subjectClass)
-	{
-		Object result;
+    public Object getAdaptor(Class subjectClass)
+    {
+        Object result;
 
-		if (CAT.isDebugEnabled())
-			CAT.debug("Getting adaptor for class " + subjectClass.getName());
+        if (CAT.isDebugEnabled())
+            CAT.debug("Getting adaptor for class " + subjectClass.getName());
 
-		synchronized (cache)
-		{
-			result = cache.get(subjectClass);
+        synchronized (cache)
+        {
+            result = cache.get(subjectClass);
 
-			if (result != null)
-			{
-				if (CAT.isDebugEnabled())
-					CAT.debug("Found " + result + " in cache");
+            if (result != null)
+            {
+                if (CAT.isDebugEnabled())
+                    CAT.debug("Found " + result + " in cache");
 
-				return result;
-			}
-		}
+                return result;
+            }
+        }
 
-			result = searchForAdaptor(subjectClass);
+        result = searchForAdaptor(subjectClass);
 
-			// Record the result in the cache
+        // Record the result in the cache
 
-			synchronized (cache)
-			{
-				cache.put(subjectClass, result);
-			}
+        synchronized (cache)
+        {
+            cache.put(subjectClass, result);
+        }
 
-				if (CAT.isDebugEnabled())
-					CAT.debug("Found " + result);
+        if (CAT.isDebugEnabled())
+            CAT.debug("Found " + result);
 
-		return result;
-	}
+        return result;
+    }
 
-	/**
-	 * Searches the registration Map for a match, based on inheritance.
-	 *
-	 * <p>Searches class inheritance first, then interfaces (in a rather vague order).
-	 * Really should match the order from the JVM spec.
-	 *
-	 * <p>There's a degenerate case where we may check the same interface more than once:
-	 * <ul>
-	 * <li>Two interfaces, I1 and I2
-	 * <li>Two classes, C1 and C2
-	 * <li>I2 extends I1
-	 * <li>C2 extends C1
-	 * <li>C1 implements I1
-	 * <li>C2 implements I2
-	 * <li>The search will be: C2, C1, I2, I1, I1
-	 * <li>I1 is searched twice, because C1 implements it, and I2 extends it
-	 * <li>There are other such cases, but none of them cause infinite loops
-	 * and most are rare (we could guard against it, but its relatively expensive).
-	 * <li>Multiple checks only occur if we don't find a registration
-	 * </ul>
-	 *
-	 **/
+    /**
+     * Searches the registration Map for a match, based on inheritance.
+     *
+     * <p>Searches class inheritance first, then interfaces (in a rather vague order).
+     * Really should match the order from the JVM spec.
+     *
+     * <p>There's a degenerate case where we may check the same interface more than once:
+     * <ul>
+     * <li>Two interfaces, I1 and I2
+     * <li>Two classes, C1 and C2
+     * <li>I2 extends I1
+     * <li>C2 extends C1
+     * <li>C1 implements I1
+     * <li>C2 implements I2
+     * <li>The search will be: C2, C1, I2, I1, I1
+     * <li>I1 is searched twice, because C1 implements it, and I2 extends it
+     * <li>There are other such cases, but none of them cause infinite loops
+     * and most are rare (we could guard against it, but its relatively expensive).
+     * <li>Multiple checks only occur if we don't find a registration
+     * </ul>
+     *
+     **/
 
-	private Object searchForAdaptor(Class subjectClass)
-	{
-		LinkedList queue = null;
-		Class[] interfaces;
-		Class searchClass;
-		Object result;
-		int length;
+    private Object searchForAdaptor(Class subjectClass)
+    {
+        LinkedList queue = null;
+        Class[] interfaces;
+        Class searchClass;
+        Object result;
+        int length;
 
-		if (registrations == null)
-			throw new IllegalArgumentException("No adaptors have been registered.");
+        if (registrations == null)
+            throw new IllegalArgumentException("No adaptors have been registered.");
 
-		if (CAT.isDebugEnabled())
-			CAT.debug("Searching for adaptor for class " + subjectClass.getName());
+        if (CAT.isDebugEnabled())
+            CAT.debug("Searching for adaptor for class " + subjectClass.getName());
 
-		synchronized (registrations)
-		{
-			// Step one: work up through the class inheritance.
+        synchronized (registrations)
+        {
+            // Step one: work up through the class inheritance.
 
-			searchClass = subjectClass;
+            searchClass = subjectClass;
 
-			// Primitive types have null, not Object, as their parent
-			// class.
+            // Primitive types have null, not Object, as their parent
+            // class.
 
-			while (searchClass != Object.class && searchClass != null)
-			{
-				result = registrations.get(searchClass);
-				if (result != null)
-					return result;
+            while (searchClass != Object.class && searchClass != null)
+            {
+                result = registrations.get(searchClass);
+                if (result != null)
+                    return result;
 
-				// Not an exact match.  If the search class
-				// implements any interfaces, add them to the queue.
+                // Not an exact match.  If the search class
+                // implements any interfaces, add them to the queue.
 
-				interfaces = searchClass.getInterfaces();
-				length = interfaces.length;
+                interfaces = searchClass.getInterfaces();
+                length = interfaces.length;
 
-				if (queue == null && length > 0)
-					queue = new LinkedList();
+                if (queue == null && length > 0)
+                    queue = new LinkedList();
 
-				for (int i = 0; i < length; i++)
-					queue.addLast(interfaces[i]);
+                for (int i = 0; i < length; i++)
+                    queue.addLast(interfaces[i]);
 
-				// Advance up to the next superclass
+                // Advance up to the next superclass
 
-				searchClass = searchClass.getSuperclass();
+                searchClass = searchClass.getSuperclass();
 
-			}
+            }
 
-			// Ok, the easy part failed, lets start searching
-			// interfaces.
+            // Ok, the easy part failed, lets start searching
+            // interfaces.
 
-			if (queue != null)
-			{
-				while (!queue.isEmpty())
-				{
-					searchClass = (Class) queue.removeFirst();
+            if (queue != null)
+            {
+                while (!queue.isEmpty())
+                {
+                    searchClass = (Class) queue.removeFirst();
 
-					result = registrations.get(searchClass);
-					if (result != null)
-						return result;
+                    result = registrations.get(searchClass);
+                    if (result != null)
+                        return result;
 
-					// Interfaces can extend other interfaces; add them
-					// to the queue.
+                    // Interfaces can extend other interfaces; add them
+                    // to the queue.
 
-					interfaces = searchClass.getInterfaces();
-					length = interfaces.length;
+                    interfaces = searchClass.getInterfaces();
+                    length = interfaces.length;
 
-					for (int i = 0; i < length; i++)
-						queue.addLast(interfaces[i]);
-				}
-			}
+                    for (int i = 0; i < length; i++)
+                        queue.addLast(interfaces[i]);
+                }
+            }
 
-			// Not a match on interface; our last gasp is to check
-			// for a registration for java.lang.Object
+            // Not a match on interface; our last gasp is to check
+            // for a registration for java.lang.Object
 
-			result = registrations.get(Object.class);
-			if (result != null)
-				return result;
+            result = registrations.get(Object.class);
+            if (result != null)
+                return result;
 
-		}
+        }
 
-			// No match?  That's rare ... and an error.
+        // No match?  That's rare ... and an error.
 
-			throw new IllegalArgumentException(
-				Tapestry.getString("Decorator.adaptor-not-found", subjectClass.getName()));
-	}
+        throw new IllegalArgumentException(
+            Tapestry.getString("Decorator.adaptor-not-found", subjectClass.getName()));
+    }
 
-	public String toString()
-	{
-		StringBuffer buffer;
-		Iterator i;
-		Map.Entry entry;
-		boolean first = true;
-		Class registeredClass;
+    public String toString()
+    {
+        StringBuffer buffer;
+        Iterator i;
+        Map.Entry entry;
+        boolean first = true;
+        Class registeredClass;
 
-		buffer = new StringBuffer();
-		buffer.append("Decorator[");
+        buffer = new StringBuffer();
+        buffer.append("Decorator[");
 
-		if (registrations != null)
-		{
-			synchronized (registrations)
-			{
-				i = registrations.entrySet().iterator();
+        if (registrations != null)
+        {
+            synchronized (registrations)
+            {
+                i = registrations.entrySet().iterator();
 
-				while (i.hasNext())
-				{
-					if (!first)
-						buffer.append(' ');
+                while (i.hasNext())
+                {
+                    if (!first)
+                        buffer.append(' ');
 
-					entry = (Map.Entry) i.next();
+                    entry = (Map.Entry) i.next();
 
-					registeredClass = (Class) entry.getKey();
+                    registeredClass = (Class) entry.getKey();
 
-					buffer.append(registeredClass.getName());
-					buffer.append("=");
-					buffer.append(entry.getValue());
+                    buffer.append(registeredClass.getName());
+                    buffer.append("=");
+                    buffer.append(entry.getValue());
 
-					first = false;
-				}
-			}
-		}
+                    first = false;
+                }
+            }
+        }
 
-		buffer.append("]");
+        buffer.append("]");
 
-		return buffer.toString();
+        return buffer.toString();
 
-	}
+    }
 }
