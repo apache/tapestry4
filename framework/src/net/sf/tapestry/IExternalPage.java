912a389b7d279a39f79e32862d5cb184679be0e9
malcolm_edgar <dev-null@apache.org>
2002-09-02 12:24:36 +0000

    [603132] Added external service and external page derived from vlib
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/tapestry/trunk@242789 13f79535-47bb-0310-9956-ffa450edef68

---
+++ framework/src/net/sf/tapestry/IExternalPage.java
@@ -0,0 +1,55 @@
+//
+// Tapestry Web Application Framework
+// Copyright (c) 2000-2002 by Howard Lewis Ship
+//
+// Howard Lewis Ship
+// http://sf.net/projects/tapestry
+// mailto:hship@users.sf.net
+//
+// This library is free software.
+//
+// You may redistribute it and/or modify it under the terms of the GNU
+// Lesser General Public License as published by the Free Software Foundation.
+//
+// Version 2.1 of the license should be included with this distribution in
+// the file LICENSE, as well as License.html. If the license is not
+// included with this distribution, you may find a copy at the FSF web
+// site at 'www.gnu.org' or 'www.fsf.org', or you may write to the
+// Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139 USA.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied waranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+
+package net.sf.tapestry;
+
+import net.sf.tapestry.IPage;
+import net.sf.tapestry.IRequestCycle;
+
+/**
+ * Defines an page which may be referenced externally via a URL using the 
+ * {@link net.sf.tapestry.engine.ExternalService}. External pages may be bookmarked 
+ * via their URL for latter display. See the 
+ * {@link net.sf.tapestry.engine.ExternalService} for details on how to invoke
+ * <tt>IExternalPage</tt>s.
+ * 
+ * @see net.sf.tapestry.engine.ExternalService
+ *
+ * @author Howard Lewis Ship
+ * @author Malcolm Edgar
+ * @version $Id$
+ * @since 2.2
+ **/
+public interface IExternalPage extends IPage
+{
+    /**
+     * Setup the external page with the given array of parameters and
+     * request cycle
+     *
+     * @param parameters the array of page parameters
+     * @param cycle the page's <tt>IRequestCycle</tt>
+     */
+    public void setup(Object[] parameters, IRequestCycle cycle);
+}
