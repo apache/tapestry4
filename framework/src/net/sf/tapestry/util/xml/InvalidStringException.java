e68d46632443d5cdb26f867c9e1b2a1293001c33
Geoff Longman <glongman@apache.org>
2002-09-21 20:30:02 +0000

    Added InvalidStringException as a subclass of DocumentParseException Modified AbstractDocumentParser to support the new Exception type This was done to allow better error handling in Spindle
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/tapestry/trunk@242901 13f79535-47bb-0310-9956-ffa450edef68

---
+++ framework/src/net/sf/tapestry/util/xml/InvalidStringException.java
@@ -0,0 +1,56 @@
+//
+// Tapestry Web Application Framework
+// Copyright (c) 2000-2002 by Howard Lewis Ship
+//
+// Howard Lewis Ship
+// http://sf.net/projects/tapestry
+// mailto:hship@users.sf.net
+//
+// This library is free software.
+//
+// You may redistribute it and/or modify it under the terms of the GNU
+// Lesser General Public License as published by the Free Software Foundation.
+//
+// Version 2.1 of the license should be included with this distribution in
+// the file LICENSE, as well as License.html. If the license is not
+// included with this distribution, you may find a copy at the FSF web
+// site at 'www.gnu.org' or 'www.fsf.org', or you may write to the
+// Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139 USA.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied waranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+package net.sf.tapestry.util.xml;
+
+import org.xml.sax.SAXParseException;
+
+/**
+ *  Exception thrown if there is any kind of error validating a string
+ *  during document parsing
+ *
+ *  @see AbstractDocumentParser
+ *
+ *  @author Geoffrey Longman
+ *  @version $Id$
+ *  @since 2.2
+ *
+ **/
+public class InvalidStringException extends DocumentParseException {
+
+  private String invalidString;
+
+  public InvalidStringException(String message, String invalidString, String resourcePath) 
+  {
+    super(message, resourcePath);
+    this.invalidString = invalidString;
+  }
+
+ 
+  public String getInvalidString() 
+  {
+    return invalidString;
+  }
+
+}
