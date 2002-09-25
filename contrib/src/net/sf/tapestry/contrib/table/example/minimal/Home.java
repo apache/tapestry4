e4a7131bf9eefb551b8e3fa01cc8f21f51dce00d
Mind Bridge / Teodor Georgiev Toncher <mindbridge@apache.org>
2002-09-25 07:26:44 +0000

    using the ExpressionTableColumnModel
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/tapestry/trunk@242920 13f79535-47bb-0310-9956-ffa450edef68

--- contrib/src/net/sf/tapestry/contrib/table/example/minimal/Home.java
+++ contrib/src/net/sf/tapestry/contrib/table/example/minimal/Home.java
@@ -28,7 +28,11 @@ package net.sf.tapestry.contrib.table.example.minimal;
 import java.util.Locale;
 
 import net.sf.tapestry.contrib.table.model.ITableColumn;
+import net.sf.tapestry.contrib.table.model.ITableColumnModel;
+import net.sf.tapestry.contrib.table.model.ITableDataModel;
 import net.sf.tapestry.contrib.table.model.ITableModel;
+import net.sf.tapestry.contrib.table.model.ognl.ExpressionTableColumnModel;
+import net.sf.tapestry.contrib.table.model.simple.SimpleListTableDataModel;
 import net.sf.tapestry.contrib.table.model.simple.SimpleTableColumn;
 import net.sf.tapestry.contrib.table.model.simple.SimpleTableModel;
 import net.sf.tapestry.html.BasePage;
@@ -43,48 +47,22 @@ public class Home extends BasePage
 
 	public ITableModel getTableModel()
 	{
-		// Generate the list of data
+		// Generate the list of data and use a simple data model
 		Locale[] arrLocales = Locale.getAvailableLocales();
+        ITableDataModel objDataModel = new SimpleListTableDataModel(arrLocales);
 
-		// Generate the list of columns
-		ITableColumn[] arrColumns =
-			new ITableColumn[] { new SimpleTableColumn("Locale", true)
-			{ public Object getColumnValue(Object objRow)
-				{ return ((Locale) objRow).toString();
-				}
-			}, new SimpleTableColumn("Language", true)
-			{
-				public Object getColumnValue(Object objRow)
-				{
-					return ((Locale) objRow).getLanguage();
-				}
-			}, new SimpleTableColumn("Country", true)
-			{
-				public Object getColumnValue(Object objRow)
-				{
-					return ((Locale) objRow).getCountry();
-				}
-			}, new SimpleTableColumn("Variant", true)
-			{
-				public Object getColumnValue(Object objRow)
-				{
-					return ((Locale) objRow).getVariant();
-				}
-			}, new SimpleTableColumn("ISO Language", true)
-			{
-				public Object getColumnValue(Object objRow)
-				{
-					return ((Locale) objRow).getISO3Language();
-				}
-			}, new SimpleTableColumn("ISO Country", true)
-			{
-				public Object getColumnValue(Object objRow)
-				{
-					return ((Locale) objRow).getISO3Country();
-				}
-			}, };
+        // Generate a simple column model that uses OGNL to get the column data
+        ITableColumnModel objColumnModel = 
+            new ExpressionTableColumnModel(new String[] {
+                "Locale", "toString()",
+                "Language", "language",
+                "Country", "country",
+                "Variant", "variant",
+                "ISO Language", "ISO3Language",
+                "ISO Country", "ISO3Country"
+            }, true);
 
 		// Create the model and return it
-		return new SimpleTableModel(arrLocales, arrColumns);
+		return new SimpleTableModel(objDataModel, objColumnModel);
 	}
 }
