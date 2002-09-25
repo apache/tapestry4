682157c1c5247dd7a1224fd8f7aa2e7ffc563e65
hship <dev-null@apache.org>
2002-09-25 21:49:23 +0000

    Update to Jetty 4.1.1.
    git-svn-id: https://svn.eu.apache.org/repos/asf/jakarta/tapestry/trunk@242932 13f79535-47bb-0310-9956-ffa450edef68

--- contrib/src/net/sf/tapestry/contrib/informal/InheritInformalAny.java
+++ contrib/src/net/sf/tapestry/contrib/informal/InheritInformalAny.java
@@ -38,80 +38,88 @@ import net.sf.tapestry.RequestCycleException;
 import net.sf.tapestry.spec.ComponentSpecification;
 
 /**
- * A version of the Any component that inherits the informal attributes of its parent
  * 
- * @version $Id$
- * @author mindbridge
- */
-public class InheritInformalAny extends AbstractComponent 
+ *  A version of the Any component that inherits the informal attributes of its parent
+ * 
+ *  @version $Id$
+ *  @author mindbridge
+ *  @since 2.2
+ * 
+ **/
+
+public class InheritInformalAny extends AbstractComponent
 {
     // Bindings
-	private IBinding m_objElementBinding;
+    private IBinding m_objElementBinding;
 
-	public IBinding getElementBinding() {
-		return m_objElementBinding;
-	}
-
-	public void setElementBinding(IBinding objElementBinding) {
-		m_objElementBinding = objElementBinding;
-	}
-
-	protected void generateParentAttributes(IMarkupWriter writer, IRequestCycle cycle) {
-		String attribute;
-
-		IComponent objParent = getContainer();
-		if (objParent == null)
-			return;
-
-		ComponentSpecification specification = objParent.getSpecification();
-		Map bindings = objParent.getBindings();
-		if (bindings == null)
-			return;
-
-		Iterator i = bindings.entrySet().iterator();
-
-		while (i.hasNext()) {
-			Map.Entry entry = (Map.Entry) i.next();
-			String name = (String) entry.getKey();
-
-			// Skip over formal parameters stored in the bindings
-			// Map.  We're just interested in informal parameters.
-
-			if (specification.getParameter(name) != null)
-				continue;
-
-			IBinding binding = (IBinding) entry.getValue();
-
-			Object value = binding.getObject();
-			if (value == null)
-				continue;
-
-			if (value instanceof IAsset) {
-				IAsset asset = (IAsset) value;
-
-				// Get the URL of the asset and insert that.
-				attribute = asset.buildURL(cycle);
-			}
-			else
-				attribute = value.toString();
-
-			writer.attribute(name, attribute);
-		}
-
-	}
-
-	public void renderComponent(IMarkupWriter writer, IRequestCycle cycle)
-		throws RequestCycleException 
+    public IBinding getElementBinding()
     {
-		String strElement = m_objElementBinding.getObject().toString();
+        return m_objElementBinding;
+    }
 
-		writer.begin(strElement);
-		generateParentAttributes(writer, cycle);
-		generateAttributes(writer, cycle);
+    public void setElementBinding(IBinding objElementBinding)
+    {
+        m_objElementBinding = objElementBinding;
+    }
 
-		renderWrapped(writer, cycle);
+    protected void generateParentAttributes(IMarkupWriter writer, IRequestCycle cycle)
+    {
+        String attribute;
 
-		writer.end();
-	}
+        IComponent objParent = getContainer();
+        if (objParent == null)
+            return;
+
+        ComponentSpecification specification = objParent.getSpecification();
+        Map bindings = objParent.getBindings();
+        if (bindings == null)
+            return;
+
+        Iterator i = bindings.entrySet().iterator();
+
+        while (i.hasNext())
+        {
+            Map.Entry entry = (Map.Entry) i.next();
+            String name = (String) entry.getKey();
+
+            // Skip over formal parameters stored in the bindings
+            // Map.  We're just interested in informal parameters.
+
+            if (specification.getParameter(name) != null)
+                continue;
+
+            IBinding binding = (IBinding) entry.getValue();
+
+            Object value = binding.getObject();
+            if (value == null)
+                continue;
+
+            if (value instanceof IAsset)
+            {
+                IAsset asset = (IAsset) value;
+
+                // Get the URL of the asset and insert that.
+                attribute = asset.buildURL(cycle);
+            }
+            else
+                attribute = value.toString();
+
+            writer.attribute(name, attribute);
+        }
+
+    }
+
+    public void renderComponent(IMarkupWriter writer, IRequestCycle cycle) throws RequestCycleException
+    {
+        String strElement = m_objElementBinding.getObject().toString();
+
+        writer.begin(strElement);
+        generateParentAttributes(writer, cycle);
+        generateAttributes(writer, cycle);
+
+        renderBody(writer, cycle);
+
+        writer.end();
+    }
 
 }
