b245ef975a858772f5aa211a93d5e7ae88f79e28
scornflake <dev-null@apache.org>
2002-11-24 08:46:39 +0000

    Added basic component application (just a shell at present)
    git-svn-id: https://svn.eu.apache.org/repos/asf/jakarta/tapestry/trunk@243035 13f79535-47bb-0310-9956-ffa450edef68

---
+++ examples/Tutorial2/src/tutorial/components/Home.java
@@ -0,0 +1,10 @@
+package tutorial.components;
+
+import net.sf.tapestry.html.BasePage;
+
+/**
+ * Example code for the "creating components" section of the tutorial
+ * @author neil clayton
+ */
+public class Home extends BasePage {
+}
