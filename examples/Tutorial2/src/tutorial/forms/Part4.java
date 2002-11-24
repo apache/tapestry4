c50128670e4d98b16e2d378907e4dbd6b1fe20a9
scornflake <dev-null@apache.org>
2002-11-24 08:28:01 +0000

    Added code for tutorial chapters five and six
    git-svn-id: https://svn.eu.apache.org/repos/asf/jakarta/tapestry/trunk@243031 13f79535-47bb-0310-9956-ffa450edef68

--- examples/Tutorial2/src/tutorial/forms/Part4.java
+++ examples/Tutorial2/src/tutorial/forms/Part4.java
@@ -27,6 +27,8 @@ package tutorial.forms;
 
 import net.sf.tapestry.IRequestCycle;
 import net.sf.tapestry.RequestCycleException;
+import net.sf.tapestry.form.IPropertySelectionModel;
+import net.sf.tapestry.form.StringPropertySelectionModel;
 import net.sf.tapestry.html.BasePage;
 import net.sf.tapestry.valid.ValidationDelegate;
 
@@ -37,13 +39,20 @@ import net.sf.tapestry.valid.ValidationDelegate;
 public class Part4 extends BasePage {
 	public void enterDetails(IRequestCycle cycle) throws RequestCycleException {
 		// Submission has been performed. Validate the fields
-		ValidationDelegate delegate = (ValidationDelegate)getBeans().getBean("delegate");
-		if(delegate.getHasErrors()) {
+		ValidationDelegate delegate = (ValidationDelegate) getBeans().getBean("delegate");
+		if (delegate.getHasErrors()) {
 			// there are errors
 			return;
 		}
-		
+
 		cycle.setPage("Success");
 	}
-	
+
+	/**
+	 * Returns a set of colours that the user may choose from.
+	 */
+	public static IPropertySelectionModel getColourModel() {
+		return new StringPropertySelectionModel(
+			new String[] { "Black", "Fiji Blue", "Green", "Red", "Yellow" });
+	}
 }
