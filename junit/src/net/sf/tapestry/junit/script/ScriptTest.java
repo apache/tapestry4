3a19da49b9e2b4f911e53e96b8c96e8e8c3514bd
hship <dev-null@apache.org>
2002-11-27 17:58:58 +0000

    Switch license from LGPL to ASL.
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/tapestry/trunk@243050 13f79535-47bb-0310-9956-ffa450edef68

--- junit/src/net/sf/tapestry/junit/script/ScriptTest.java
+++ junit/src/net/sf/tapestry/junit/script/ScriptTest.java
@@ -1,30 +1,3 @@
-/* ----------------------------------------------------------------------
- * Copyright (c) 2002 by WebCT.
- * All Rights Reserved
- *
- * Permission to use this program (WebCT) and its related files is at the
- * discretion of WebCT.
- *
- * The licensee of WebCT agrees that:
- *    - Redistribution in whole or in part is not permitted.
- *    - Modification of source is not permitted.
- *    - Use of the source in whole or in part outside of WebCT is not
- *      permitted.
- *
- * THIS SOFTWARE IS PROVIDED ''AS IS'' AND ANY EXPRESSED OR IMPLIED
- * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
- * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
- * IN NO EVENT SHALL WEBCT OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
- * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
- * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
- * OF THE POSSIBILITY OF SUCH DAMAGE.
- * ----------------------------------------------------------------------
- */
-
 package net.sf.tapestry.junit.script;
 
 import java.io.IOException;
