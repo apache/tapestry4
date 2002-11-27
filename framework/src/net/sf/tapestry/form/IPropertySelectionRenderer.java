3d4161ad467610a3e9fbe51148991ab112230ba0
hship <dev-null@apache.org>
2002-11-27 17:58:58 +0000

    Switch license from LGPL to ASL.
    git-svn-id: https://svn.eu.apache.org/repos/asf/jakarta/tapestry/trunk@243050 13f79535-47bb-0310-9956-ffa450edef68

--- framework/src/net/sf/tapestry/form/IPropertySelectionRenderer.java
+++ framework/src/net/sf/tapestry/form/IPropertySelectionRenderer.java
@@ -1,28 +1,3 @@
-//
-// Tapestry Web Application Framework
-// Copyright (c) 2000-2002 by Howard Lewis Ship
-//
-// Howard Lewis Ship
-// http://sf.net/projects/tapestry
-// mailto:hship@users.sf.net
-//
-// This library is free software.
-//
-// You may redistribute it and/or modify it under the terms of the GNU
-// Lesser General Public License as published by the Free Software Foundation.
-//
-// Version 2.1 of the license should be included with this distribution in
-// the file LICENSE, as well as License.html. If the license is not
-// included with this distribution, you may find a copy at the FSF web
-// site at 'www.gnu.org' or 'www.fsf.org', or you may write to the
-// Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139 USA.
-//
-// This library is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied waranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-// Lesser General Public License for more details.
-//
-
 package net.sf.tapestry.form;
 
 import net.sf.tapestry.IMarkupWriter;
