1c97ad55f9a917e22bea22eb165fba54903cab97
Mind Bridge / Teodor Georgiev Toncher <mindbridge@apache.org>
2002-12-23 12:44:07 +0000

    using the 2.3 Table features
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/tapestry/trunk@243119 13f79535-47bb-0310-9956-ffa450edef68

--- examples/Tutorial/src/tutorial/workbench/table/VerbosityRating.java
+++ examples/Tutorial/src/tutorial/workbench/table/VerbosityRating.java
@@ -11,73 +11,81 @@ import java.util.Locale;
  */
 public class VerbosityRating implements Serializable
 {
-    
+
 	/**
 	 * Method calculateVerbosity.
-     * Please note that this method is relatively slow
-     * It should not be used often, unless for fun :)
+	 * Please note that this method is relatively slow
+	 * It should not be used often, unless for fun :)
 	 * @param objLocale
 	 * @return int
 	 */
-    public int calculateVerbosity(Locale objLocale) {
-        int nWeekDayVerbosity = calculateWeekDayVerbosity(objLocale);
-        int nMonthVerbosity = calculateMonthVerbosity(objLocale);
-        return nWeekDayVerbosity + nMonthVerbosity;
-    }
-    
-    public int calculateWeekDayVerbosity(Locale objLocale) {
-        SimpleDateFormat objWeekDay = new SimpleDateFormat("EEEE", objLocale);
+	public static int calculateVerbosity(Locale objLocale)
+	{
+		int nWeekDayVerbosity = calculateWeekDayVerbosity(objLocale);
+		int nMonthVerbosity = calculateMonthVerbosity(objLocale);
+		return nWeekDayVerbosity + nMonthVerbosity;
+	}
 
-        GregorianCalendar objCalendar = new GregorianCalendar();
-        objCalendar.set(GregorianCalendar.YEAR, 2000);
-        objCalendar.set(GregorianCalendar.MONTH, 0);
-        objCalendar.set(GregorianCalendar.DATE, 1);
-        
-        int nCount = 0;
-        for (int i = 0; i < 7; i++) {
-            String strWeekDay = objWeekDay.format(objCalendar.getTime());
-            nCount += strWeekDay.length();
-            objCalendar.add(GregorianCalendar.DATE, 1);
-        }
-        
-        return nCount;
-    }
+	public static int calculateWeekDayVerbosity(Locale objLocale)
+	{
+		SimpleDateFormat objWeekDay = new SimpleDateFormat("EEEE", objLocale);
 
-    public int calculateMonthVerbosity(Locale objLocale) {
-        SimpleDateFormat objMonth = new SimpleDateFormat("MMMM", objLocale);
+		GregorianCalendar objCalendar = new GregorianCalendar();
+		objCalendar.set(GregorianCalendar.YEAR, 2000);
+		objCalendar.set(GregorianCalendar.MONTH, 0);
+		objCalendar.set(GregorianCalendar.DATE, 1);
 
-        GregorianCalendar objCalendar = new GregorianCalendar();
-        objCalendar.set(GregorianCalendar.YEAR, 2000);
-        objCalendar.set(GregorianCalendar.MONTH, 0);
-        objCalendar.set(GregorianCalendar.DATE, 1);
-        
-        int nCount = 0;
-        for (int i = 0; i < 12; i++) {
-            String strMonth = objMonth.format(objCalendar.getTime());
-            nCount += strMonth.length();
-            objCalendar.add(GregorianCalendar.MONTH, 1);
-        }
-        
-        return nCount;
-    }
+		int nCount = 0;
+		for (int i = 0; i < 7; i++)
+		{
+			String strWeekDay = objWeekDay.format(objCalendar.getTime());
+			nCount += strWeekDay.length();
+			objCalendar.add(GregorianCalendar.DATE, 1);
+		}
 
-    public static void main(String[] arrArgs) {
-        int nMax = 0;
-        int nMin = 1000;
-        
-        System.out.println("Starting");
-        
-        VerbosityRating objRating = new VerbosityRating();
-        Locale[] arrLocales = Locale.getAvailableLocales();
-        for (int i = 0; i < arrLocales.length; i++) {
-            Locale objLocale = arrLocales[i];
-            int nRating = objRating.calculateVerbosity(objLocale);
-            if (nRating > nMax) nMax = nRating;
-            if (nRating < nMin) nMin = nRating;
-        }
-        
-        System.out.println("Min: " + nMin);
-        System.out.println("Max: " + nMax);
-    }
+		return nCount;
+	}
+
+	public static int calculateMonthVerbosity(Locale objLocale)
+	{
+		SimpleDateFormat objMonth = new SimpleDateFormat("MMMM", objLocale);
+
+		GregorianCalendar objCalendar = new GregorianCalendar();
+		objCalendar.set(GregorianCalendar.YEAR, 2000);
+		objCalendar.set(GregorianCalendar.MONTH, 0);
+		objCalendar.set(GregorianCalendar.DATE, 1);
+
+		int nCount = 0;
+		for (int i = 0; i < 12; i++)
+		{
+			String strMonth = objMonth.format(objCalendar.getTime());
+			nCount += strMonth.length();
+			objCalendar.add(GregorianCalendar.MONTH, 1);
+		}
+
+		return nCount;
+	}
+
+	public static void main(String[] arrArgs)
+	{
+		int nMax = 0;
+		int nMin = 1000;
+
+		System.out.println("Starting");
+
+		Locale[] arrLocales = Locale.getAvailableLocales();
+		for (int i = 0; i < arrLocales.length; i++)
+		{
+			Locale objLocale = arrLocales[i];
+			int nRating = calculateVerbosity(objLocale);
+			if (nRating > nMax)
+				nMax = nRating;
+			if (nRating < nMin)
+				nMin = nRating;
+		}
+
+		System.out.println("Min: " + nMin);
+		System.out.println("Max: " + nMax);
+	}
 
 }
