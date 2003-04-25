791dc68adec2df5cc8f27bc4616106cd04d6d1e1
Howard Lewis Ship <hlship@apache.org>
2003-04-25 01:15:01 +0000

    Integrate Per Norrman's overhauled DatePicker component [ 18880 ] DatePicker broken under Mozilla
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/tapestry/trunk@243405 13f79535-47bb-0310-9956-ffa450edef68

--- framework/src/org/apache/tapestry/form/DatePicker.java
+++ framework/src/org/apache/tapestry/form/DatePicker.java
@@ -55,16 +55,23 @@
 
 package org.apache.tapestry.form;
 
-import java.text.DateFormat;
+import java.text.DateFormatSymbols;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
 
-import org.apache.tapestry.IAsset;
+import org.apache.tapestry.IEngine;
 import org.apache.tapestry.IForm;
 import org.apache.tapestry.IMarkupWriter;
 import org.apache.tapestry.IRequestCycle;
+import org.apache.tapestry.IResourceLocation;
+import org.apache.tapestry.IScript;
 import org.apache.tapestry.Tapestry;
+import org.apache.tapestry.engine.IScriptSource;
 import org.apache.tapestry.html.Body;
 
 /**
@@ -89,10 +96,41 @@ public abstract class DatePicker extends AbstractFormComponent
 
     public abstract boolean isDisabled();
 
+    public abstract boolean getIncludeWeek();
+
+    private IScript _script;
+
+    private static final String SYM_NAME = "name";
+    private static final String SYM_FORMNAME = "formName";
+    private static final String SYM_MONTHNAMES = "monthNames";
+    private static final String SYM_SHORT_MONTHNAMES = "shortMonthNames";
+    private static final String SYM_WEEKDAYNAMES = "weekDayNames";
+    private static final String SYM_SHORT_WEEKDAYNAMES = "shortWeekDayNames";
+    private static final String SYM_FIRSTDAYINWEEK = "firstDayInWeek";
+    private static final String SYM_MINDAYSINFIRSTWEEK = "minimalDaysInFirstWeek";
+    private static final String SYM_FORMAT = "format";
+    private static final String SYM_INCL_WEEK = "includeWeek";
+    private static final String SYM_VALUE = "value";
+
+    // Output symbol
+
+    private static final String SYM_BUTTONNAME = "buttonName";
+
+    protected void finishLoad()
+    {
+        IEngine engine = getPage().getEngine();
+        IScriptSource source = engine.getScriptSource();
+
+        IResourceLocation location =
+            getSpecification().getSpecificationLocation().getRelativeLocation("DatePicker.script");
+
+        _script = source.getScript(location);
+    }
+
     protected void renderComponent(IMarkupWriter writer, IRequestCycle cycle)
     {
         IForm form = getForm(cycle);
-		
+
         String name = form.getElementId(this);
 
         String format = getFormat();
@@ -100,7 +138,7 @@ public abstract class DatePicker extends AbstractFormComponent
         if (format == null)
             format = "dd MMM yyyy";
 
-        DateFormat formatter = new SimpleDateFormat(format);
+        SimpleDateFormat formatter = new SimpleDateFormat(format);
 
         boolean disabled = isDisabled();
 
@@ -108,17 +146,35 @@ public abstract class DatePicker extends AbstractFormComponent
         {
             Body body = Body.get(cycle);
 
-            IAsset script = getAsset("script");
+            Locale locale = getPage().getLocale();
+            DateFormatSymbols dfs = new DateFormatSymbols(locale);
+            Calendar cal = Calendar.getInstance(locale);
 
-            body.includeScript(script.buildURL(cycle));
+			Date value = getValue();
+			
+            Map symbols = new HashMap();
 
-            Date value = getValue();
+            symbols.put(SYM_NAME, name);
+            symbols.put(SYM_FORMAT, format);
+            symbols.put(SYM_INCL_WEEK, getIncludeWeek() ? Boolean.TRUE : Boolean.FALSE);
+
+            symbols.put(SYM_MONTHNAMES, makeStringList(dfs.getMonths(), 0, 12));
+            symbols.put(SYM_SHORT_MONTHNAMES, makeStringList(dfs.getShortMonths(), 0, 12));
+            symbols.put(SYM_WEEKDAYNAMES, makeStringList(dfs.getWeekdays(), 1, 8));
+            symbols.put(SYM_SHORT_WEEKDAYNAMES, makeStringList(dfs.getShortWeekdays(), 1, 8));
+            symbols.put(SYM_FIRSTDAYINWEEK, new Integer(cal.getFirstDayOfWeek() - 1));
+            symbols.put(SYM_MINDAYSINFIRSTWEEK, new Integer(cal.getMinimalDaysInFirstWeek()));
+            symbols.put(SYM_FORMNAME, form.getName());
+            symbols.put(SYM_VALUE, value);
+
+            _script.execute(cycle, body, symbols);
 
             writer.beginEmpty("input");
             writer.attribute("type", "text");
             writer.attribute("name", name);
-            writer.attribute("maxlength", format.length());
-            writer.attribute("size", format.length());
+            writer.attribute("maxlength", format.length() + 2);
+            writer.attribute("size", format.length() + 2);
+            writer.attribute("title", formatter.toLocalizedPattern());
 
             if (value != null)
                 writer.attribute("value", formatter.format(value));
@@ -126,29 +182,19 @@ public abstract class DatePicker extends AbstractFormComponent
             if (disabled)
                 writer.attribute("disabled", "disabled");
 
-            writer.beginEmpty("input");
-            writer.attribute("type", "hidden");
-            writer.attribute("name", name + "$millis");
-
             if (value == null)
                 writer.attribute("value", "");
             else
                 writer.attribute("value", Long.toString(value.getTime()));
 
             writer.beginEmpty("input");
-            writer.attribute("type", "hidden");
-            writer.attribute("name", name + "$format");
-            writer.attribute("value", format);
-
-            writer.beginEmpty("input");
             writer.attribute("type", "button");
-            writer.attribute("name", name + "$button");
+            writer.attribute("name", (String) symbols.get(SYM_BUTTONNAME));
 
             if (disabled)
                 writer.attribute("disabled", "disabled");
 
             writer.attribute("value", "V");
-            writer.attribute("onClick", "javascript:goCalendar(this);");
         }
 
         if (form.isRewinding())
@@ -174,5 +220,56 @@ public abstract class DatePicker extends AbstractFormComponent
 
     }
 
+    /**
+     * Create a list of quoted strings. The list is suitable for
+     * initializing a JavaScript array.
+     */
+    private String makeStringList(String[] a, int offset, int length)
+    {
+        StringBuffer b = new StringBuffer();
+        for (int i = offset; i < length; i++)
+        {
+            // JavaScript is sensitive to some UNICODE characters. So for
+            // the sake of simplicity, we just escape everything
+            b.append('"');
+            char[] ch = a[i].toCharArray();
+            for (int j = 0; j < ch.length; j++)
+            {
+                if (ch[j] < 128)
+                {
+                    b.append(ch[j]);
+                }
+                else
+                {
+                    b.append(escape(ch[j]));
+                }
+            }
+
+            b.append('"');
+            if (i < length - 1)
+            {
+                b.append(", ");
+            }
+        }
+        return b.toString();
+
+    }
+
+    /**
+     * Create an escaped Unicode character
+     * @param c
+     * @return The unicode character in escaped string form
+     */
+    private static String escape(char c)
+    {
+        StringBuffer b = new StringBuffer();
+        for (int i = 0; i < 4; i++)
+        {
+            b.append(Integer.toHexString(c & 0x000F).toUpperCase());
+            c >>>= 4;
+        }
+        b.append("u\\");
+        return b.reverse().toString();
+    }
 
 }
