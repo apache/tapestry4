8c5737a4837a746bd2efccb536552bed14592d47
Howard Lewis Ship <hlship@apache.org>
2003-05-04 20:36:17 +0000

    Add a Servlet 2.3 API Filter for redirecting "naked" servlet context URLs to the Tapestry application servlet.
    git-svn-id: https://svn.eu.apache.org/repos/asf/jakarta/tapestry/trunk@243424 13f79535-47bb-0310-9956-ffa450edef68

---
+++ framework/src/org/apache/tapestry/RedirectFilter.java
@@ -0,0 +1,95 @@
+package org.apache.tapestry;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/**
+ * Filter used to redirect a root context URL (i.e., "/context" or "/context/"
+ * to the Tapestry application servlet (typically, "/context/app").  This
+ * servlet is mapped to "/" and must have a &lt;init-parameter&;gt; 
+ * <code>redirect-path</code> that is the application servlet's path (i.e.,
+ * "/app").  If no value is specified, then "/app" is used.  The path
+ * is always relative to the servlet context, and should always
+ * begin with a leading slash.
+ *  
+ * <p>Filters are only available in Servlet API 2.3 and above.
+ * 
+ * <p>Servlet API 2.4 is expected to allow a servlets in the welcome list
+ * (equivalent to index.html or index.jsp), at which point this filter
+ * should no longer be necessary.
+ *
+ * @author Howard Lewis Ship
+ * @version $Id$
+ * @since 3.0
+ */
+
+public class RedirectFilter implements Filter
+{
+    private static final Log LOG = LogFactory.getLog(RedirectFilter.class);
+    public static final String REDIRECT_PATH_PARAM = "redirect-path";
+
+    private String _redirectPath;
+
+    public void init(FilterConfig config) throws ServletException
+    {
+        _redirectPath = config.getInitParameter(REDIRECT_PATH_PARAM);
+
+        if (Tapestry.isNull(_redirectPath))
+            _redirectPath = "/app";
+
+        if (LOG.isInfoEnabled())
+            LOG.info(Tapestry.getString("RedirectServlet.redirect-path", _redirectPath));
+    }
+
+    public void destroy()
+    {
+
+    }
+
+    /**
+     * This filter intercepts the so-called "default" servlet, whose job is
+     * to provide access to standard resources packaged within the web application
+     * context.  This code is interested in only the very root, redirecting
+     * to the appropriate Tapestry application servlet.  Other values
+     * are passed through unchanged.
+     */
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+        throws IOException, ServletException
+    {
+        HttpServletRequest hrequest = (HttpServletRequest) request;
+        HttpServletResponse hresponse = (HttpServletResponse) response;
+
+        String servletPath = hrequest.getServletPath();
+        String pathInfo = hrequest.getPathInfo();
+
+        // Been experimenting with different servlet containers.  In Jetty 4.2.8 and Tomcat 4.1,
+        // resources have a non-null servletPath.  If JBossWeb 3.0.6, the servletPath is
+        // null and the pathInfo indicates the relative location of the resource.
+
+        if ((Tapestry.isNull(servletPath) || servletPath.equals("/"))
+            && (Tapestry.isNull(pathInfo) || pathInfo.equals("/")))
+        {
+            String path = hrequest.getContextPath() + _redirectPath;
+
+            if (LOG.isInfoEnabled())
+                LOG.info(Tapestry.getString("RedirectServlet.redirecting", path));
+
+            hresponse.sendRedirect(path);
+            return;
+        }
+
+        chain.doFilter(request, response);
+    }
+
+}
