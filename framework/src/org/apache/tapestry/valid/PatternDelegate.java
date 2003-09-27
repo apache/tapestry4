a28f3ed3fb8c2cb4af197da28639eb12ae9aafb8
Harish Krishnaswamy <harishkswamy@apache.org>
2003-09-27 16:16:27 +0000

    Initial. The implementations for this interface will provide pattern matching for the pattern validator.
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/tapestry/trunk@243570 13f79535-47bb-0310-9956-ffa450edef68

---
+++ framework/src/org/apache/tapestry/valid/PatternDelegate.java
@@ -0,0 +1,83 @@
+/* ====================================================================
+ * The Apache Software License, Version 1.1
+ *
+ * Copyright (c) 2000-2003 The Apache Software Foundation.  All rights
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The end-user documentation included with the redistribution,
+ *    if any, must include the following acknowledgment:
+ *       "This product includes software developed by the
+ *        Apache Software Foundation (http://apache.org/)."
+ *    Alternately, this acknowledgment may appear in the software itself,
+ *    if and wherever such third-party acknowledgments normally appear.
+ *
+ * 4. The names "Apache" and "Apache Software Foundation", "Tapestry" 
+ *    must not be used to endorse or promote products derived from this
+ *    software without prior written permission. For written
+ *    permission, please contact apache@apache.org.
+ *
+ * 5. Products derived from this software may not be called "Apache" 
+ *    or "Tapestry", nor may "Apache" or "Tapestry" appear in their 
+ *    name, without prior written permission of the Apache Software Foundation.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE TAPESTRY CONTRIBUTOR COMMUNITY
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation.  For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ *
+ */
+
+package org.apache.tapestry.valid;
+
+/**
+ * Implementations of this interface will provide pattern utility services.
+ * 
+ * @author  Harish Krishnaswamy
+ * @version $Id$
+ * @since   3.0
+ */
+public interface PatternDelegate
+{
+    /**
+     * Answers the question whether the input string fulfills the pattern string provided.
+     * 
+     * @param input The string under test.
+     * @param patternString The pattern that the input string is compared against.
+     * @return Returns true if the pattern exists in the input string; returns false otherwise.
+     */
+    public boolean contains(String input, String patternString);
+    
+    /**
+     * Returns the escaped sequence of characters representing the pattern string provided.
+     * 
+     * @param patternString The raw sequence of characters that represent the pattern.
+     * @return The escaped sequence of characters that represent the pattern.
+     */
+    public String getEscapedPatternString(String patternString);
+}
