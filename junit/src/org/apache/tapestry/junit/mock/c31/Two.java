dc10a0abf2ed572870eb98070d2afeebacb62a7c
Tsvetelim Saykov <tsvetelin@apache.org>
2004-02-26 16:34:35 +0000

    *** empty log message ***
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/tapestry/trunk@243806 13f79535-47bb-0310-9956-ffa450edef68

--- junit/src/org/apache/tapestry/junit/mock/c31/Two.java
+++ junit/src/org/apache/tapestry/junit/mock/c31/Two.java
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package org.apache.tapestry.junit.mock.c30;
+package org.apache.tapestry.junit.mock.c31;
 
 import org.apache.tapestry.html.BasePage;
 
