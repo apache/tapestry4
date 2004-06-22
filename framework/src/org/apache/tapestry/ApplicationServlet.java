915e24a57bc0f3ee2cca3817b48a0684e6e7a00b
Howard Lewis Ship <hlship@apache.org>
2004-06-22 13:26:01 +0000

    Begin splitting messages off of TapestryStrings.properties.
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/tapestry/trunk@243922 13f79535-47bb-0310-9956-ffa450edef68

--- framework/src/org/apache/tapestry/ApplicationServlet.java
+++ framework/src/org/apache/tapestry/ApplicationServlet.java
@@ -19,7 +19,6 @@ import java.io.InputStream;
 import java.util.Locale;
 
 import javax.servlet.ServletConfig;
-import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
@@ -30,15 +29,14 @@ import javax.servlet.http.HttpSession;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.hivemind.ClassResolver;
-import org.apache.hivemind.Resource;
+import org.apache.hivemind.Registry;
 import org.apache.hivemind.impl.DefaultClassResolver;
-import org.apache.hivemind.util.ClasspathResource;
+import org.apache.hivemind.impl.RegistryBuilder;
 import org.apache.tapestry.engine.BaseEngine;
 import org.apache.tapestry.engine.IPropertySource;
-import org.apache.tapestry.parse.SpecificationParser;
 import org.apache.tapestry.request.RequestContext;
-import org.apache.tapestry.resource.ContextResource;
-import org.apache.tapestry.spec.ApplicationSpecification;
+import org.apache.tapestry.services.ApplicationGlobals;
+import org.apache.tapestry.services.ApplicationInitializer;
 import org.apache.tapestry.spec.IApplicationSpecification;
 import org.apache.tapestry.util.DelegatingPropertySource;
 import org.apache.tapestry.util.ServletContextPropertySource;
@@ -46,7 +44,6 @@ import org.apache.tapestry.util.ServletPropertySource;
 import org.apache.tapestry.util.SystemPropertiesPropertySource;
 import org.apache.tapestry.util.exception.ExceptionAnalyzer;
 import org.apache.tapestry.util.pool.Pool;
-import org.apache.tapestry.util.xml.DocumentParseException;
 
 /**
  *  Links a servlet container with a Tapestry application.  The servlet has some
@@ -82,25 +79,22 @@ import org.apache.tapestry.util.xml.DocumentParseException;
  *  the creation of a session; this involves the servlet and the engine storing
  *  locale information in a {@link Cookie}.
  * 
- *  @version $Id$
+ * <p>
+ * As of release 3.1, this servlet will also create a HiveMind Registry and manage it.
+ * 
  *  @author Howard Lewis Ship
  * 
- **/
+ */
 
 public class ApplicationServlet extends HttpServlet
 {
     private static final Log LOG = LogFactory.getLog(ApplicationServlet.class);
 
-    /** @since 2.3 **/
-
-    private static final String APP_SPEC_PATH_PARAM =
-        "org.apache.tapestry.application-specification";
-
     /**
      *  Name of the cookie written to the client web browser to
      *  identify the locale.
      *
-     **/
+     */
 
     private static final String LOCALE_COOKIE_NAME = "org.apache.tapestry.locale";
 
@@ -108,7 +102,7 @@ public class ApplicationServlet extends HttpServlet
      *  A {@link Pool} used to store {@link IEngine engine}s that are not currently
      *  in use.  The key is on {@link Locale}.
      *
-     **/
+     */
 
     private Pool _enginePool = new Pool();
 
@@ -116,7 +110,7 @@ public class ApplicationServlet extends HttpServlet
      *  The application specification, which is read once and kept in memory
      *  thereafter.
      *
-     **/
+     */
 
     private IApplicationSpecification _specification;
 
@@ -124,7 +118,7 @@ public class ApplicationServlet extends HttpServlet
      * The name under which the {@link IEngine engine} is stored within the
      * {@link HttpSession}.
      *
-     **/
+     */
 
     private String _attributeName;
 
@@ -133,7 +127,7 @@ public class ApplicationServlet extends HttpServlet
      * 
      *  @since 3.0
      * 
-     **/
+     */
 
     private String _engineClassName;
 
@@ -143,7 +137,7 @@ public class ApplicationServlet extends HttpServlet
      *  
      *  @since 3.0
      * 
-     **/
+     */
 
     private IPropertySource _propertySource;
 
@@ -152,7 +146,7 @@ public class ApplicationServlet extends HttpServlet
      *
      *  @since 1.0.6
      *
-     **/
+     */
 
     public void doGet(HttpServletRequest request, HttpServletResponse response)
         throws IOException, ServletException
@@ -163,9 +157,15 @@ public class ApplicationServlet extends HttpServlet
     /**
      *  @since 2.3
      * 
-     **/
+     */
 
     private ClassResolver _resolver;
+
+    /**
+     * @since 3.1
+     */
+
+    private Registry _registry;
 
     /**
      * Handles the GET and POST requests. Performs the following:
@@ -174,7 +174,7 @@ public class ApplicationServlet extends HttpServlet
      * <li>Invoke {@link #getEngine(RequestContext)} to get or create the {@link IEngine}
      * <li>Invoke {@link IEngine#service(RequestContext)} on the application
      * </ul>
-     **/
+     */
 
     protected void doService(HttpServletRequest request, HttpServletResponse response)
         throws IOException, ServletException
@@ -244,9 +244,7 @@ public class ApplicationServlet extends HttpServlet
             if (engine.isStateful())
             {
                 LOG.error(
-                    Tapestry.format(
-                        "ApplicationServlet.engine-stateful-without-session",
-                        engine));
+                    Tapestry.format("ApplicationServlet.engine-stateful-without-session", engine));
                 return;
             }
 
@@ -296,7 +294,7 @@ public class ApplicationServlet extends HttpServlet
      * 
      *  @since 2.3
      * 
-     **/
+     */
 
     protected RequestContext createRequestContext(
         HttpServletRequest request,
@@ -320,7 +318,7 @@ public class ApplicationServlet extends HttpServlet
      *  Invokes {@link #doService(HttpServletRequest, HttpServletResponse)}.
      *
      *
-     **/
+     */
 
     public void doPost(HttpServletRequest request, HttpServletResponse response)
         throws IOException, ServletException
@@ -332,7 +330,7 @@ public class ApplicationServlet extends HttpServlet
      *  Returns the application specification, which is read
      *  by the {@link #init(ServletConfig)} method.
      *
-     **/
+     */
 
     public IApplicationSpecification getApplicationSpecification()
     {
@@ -348,7 +346,7 @@ public class ApplicationServlet extends HttpServlet
      *  <li>Freshly created
      *  </ul>
      *
-     **/
+     */
 
     protected IEngine getEngine(RequestContext context) throws ServletException
     {
@@ -396,7 +394,7 @@ public class ApplicationServlet extends HttpServlet
      *  from the request itself.  This may return null
      *  if no locale is determined.
      *
-     **/
+     */
 
     protected Locale getLocaleFromRequest(RequestContext context) throws ServletException
     {
@@ -417,7 +415,7 @@ public class ApplicationServlet extends HttpServlet
      *  @see #constructApplicationSpecification()
      *  @see #createResourceResolver()
      *
-     **/
+     */
 
     public void init(ServletConfig config) throws ServletException
     {
@@ -425,7 +423,19 @@ public class ApplicationServlet extends HttpServlet
 
         _resolver = createClassResolver();
 
-        _specification = constructApplicationSpecification();
+        try
+        {
+
+            constructRegistry();
+
+            initializeApplication();
+        }
+        catch (Exception ex)
+        {
+            show(ex);
+
+            throw new ServletException(TapestryMessages.servletInitFailure(ex), ex);
+        }
 
         _attributeName = "org.apache.tapestry.engine:" + config.getServletName();
     }
@@ -441,7 +451,7 @@ public class ApplicationServlet extends HttpServlet
      *  @see #getResourceResolver()
      *  @since 2.3
      * 
-     **/
+     */
 
     protected ClassResolver createClassResolver() throws ServletException
     {
@@ -449,165 +459,9 @@ public class ApplicationServlet extends HttpServlet
     }
 
     /**
-     *  Invoked from {@link #init(ServletConfig)} to read and construct
-     *  the {@link ApplicationSpecification} for this servlet.
-     *  Invokes {@link #getApplicationSpecificationPath()}, opens
-     *  the resource as a stream, then invokes
-     *  {@link #parseApplicationSpecification(IResourceLocation)}.
-     * 
-     *  <p>
-     *  This method exists to be overriden in
-     *  applications where the application specification cannot be
-     *  loaded from the classpath.  Alternately, a subclass
-     *  could override this method, invoke this implementation,
-     *  and then add additional data to it (for example, an application
-     *  where some of the pages are defined in an external source
-     *  such as a database).
-     *  
-     *  @since 2.2
-     * 
-     **/
-
-    protected IApplicationSpecification constructApplicationSpecification() throws ServletException
-    {
-        Resource specLocation = getApplicationSpecificationLocation();
-
-        if (specLocation == null)
-        {
-            if (LOG.isDebugEnabled())
-                LOG.debug(Tapestry.getMessage("ApplicationServlet.no-application-specification"));
-
-            return constructStandinSpecification();
-        }
-
-        if (LOG.isDebugEnabled())
-            LOG.debug("Loading application specification from " + specLocation);
-
-        return parseApplicationSpecification(specLocation);
-    }
-
-    /**
-     *  Gets the location of the application specification, if there is one.
-     *  
-     *  <ul>
-     *  <li>Invokes {@link #getApplicationSpecificationPath()} to get the
-     *  location of the application specification on the classpath.
-     *  <li>If that return null, search for the application specification:
-     *  <ul>
-     *  <li><i>name</i>.application in /WEB-INF/<i>name</i>/
-     *  <li><i>name</i>.application in /WEB-INF/
-     *  </ul>
-     *  </ul>
-     * 
-     *  <p>Returns the location of the application specification, or null
-     *  if not found.
-     * 
-     *  @since 3.0
-     * 
-     **/
-
-    protected Resource getApplicationSpecificationLocation() throws ServletException
-    {
-        String path = getApplicationSpecificationPath();
-
-        if (path != null)
-            return new ClasspathResource(_resolver, path);
-
-        ServletContext context = getServletContext();
-        String servletName = getServletName();
-        String expectedName = servletName + ".application";
-
-        Resource webInfLocation = new ContextResource(context, "/WEB-INF/");
-        Resource webInfAppLocation = webInfLocation.getRelativeResource(servletName + "/");
-
-        Resource result = check(webInfAppLocation, expectedName);
-        if (result != null)
-            return result;
-
-        return check(webInfLocation, expectedName);
-    }
-
-    /**
-     *  Checks for the application specification relative to the specified
-     *  location.
-     * 
-     *  @since 3.0
-     * 
-     **/
-
-    private Resource check(Resource location, String name)
-    {
-        Resource result = location.getRelativeResource(name);
-
-        if (LOG.isDebugEnabled())
-            LOG.debug("Checking for existence of " + result);
-
-        if (result.getResourceURL() != null)
-        {
-            LOG.debug("Found.");
-            return result;
-        }
-
-        return null;
-    }
-
-    /**
-     *  Invoked from {@link #constructApplicationSpecification()} when
-     *  the application doesn't have an explicit specification.  A
-     *  simple specification is constructed and returned.  This is useful
-     *  for minimal applications and prototypes.
-     * 
-     *  @since 3.0
-     * 
-     **/
-
-    protected IApplicationSpecification constructStandinSpecification()
-    {
-        ApplicationSpecification result = new ApplicationSpecification();
-
-        Resource virtualLocation =
-            new ContextResource(getServletContext(), "/WEB-INF/");
-
-        result.setSpecificationLocation(virtualLocation);
-
-        result.setName(getServletName());
-        result.setResourceResolver(_resolver);
-
-        return result;
-    }
-
-    /**
-     *  Invoked from {@link #constructApplicationSpecification()} to
-     *  actually parse the stream (with content provided from the path)
-     *  and convert it into an {@link ApplicationSpecification}.
-     * 
-     *  @since 2.2
-     * 
-     **/
-
-    protected IApplicationSpecification parseApplicationSpecification(Resource location)
-        throws ServletException
-    {
-        try
-        {
-            SpecificationParser parser = new SpecificationParser(_resolver);
-
-            return parser.parseApplicationSpecification(location);
-        }
-        catch (DocumentParseException ex)
-        {
-            show(ex);
-
-            throw new ServletException(
-                Tapestry.format("ApplicationServlet.could-not-parse-spec", location),
-                ex);
-        }
-    }
-
-    /**
      *  Closes the stream, ignoring any exceptions.
      * 
-     **/
+     */
 
     protected void close(InputStream stream)
     {
@@ -623,25 +477,6 @@ public class ApplicationServlet extends HttpServlet
     }
 
     /**
-     *  Reads the servlet init parameter
-     *  <code>org.apache.tapestry.application-specification</code>, which
-     *  is the location, on the classpath, of the application specification.
-     *
-     *  <p>
-     *  If the parameter is not set, this method returns null, and a search
-     *  for the application specification within the servlet context
-     *  will begin.
-     * 
-     *  @see #getApplicationSpecificationLocation()
-     * 
-     **/
-
-    protected String getApplicationSpecificationPath() throws ServletException
-    {
-        return getInitParameter(APP_SPEC_PATH_PARAM);
-    }
-
-    /**
      *  Invoked by {@link #getEngine(RequestContext)} to create
      *  the {@link IEngine} instance specific to the
      *  application, if not already in the
@@ -652,7 +487,7 @@ public class ApplicationServlet extends HttpServlet
      *
      *  @see #getEngineClassName()    
      *
-     **/
+     */
 
     protected IEngine createEngine(RequestContext context) throws ServletException
     {
@@ -691,7 +526,7 @@ public class ApplicationServlet extends HttpServlet
      *  <p>If the search is still unsuccessful, then
      *  {@link org.apache.tapestry.engine.BaseEngine} is used.
      * 
-     **/
+     */
 
     protected String getEngineClassName()
     {
@@ -720,7 +555,7 @@ public class ApplicationServlet extends HttpServlet
      *  @see #createPropertySource()
      *  @since 3.0
      * 
-     **/
+     */
 
     protected String searchConfiguration(String propertyName)
     {
@@ -738,7 +573,7 @@ public class ApplicationServlet extends HttpServlet
      * 
      *  @since 3.0
      * 
-     **/
+     */
 
     protected IPropertySource createPropertySource()
     {
@@ -761,7 +596,7 @@ public class ApplicationServlet extends HttpServlet
      *  change in subsequent releases.
      *
      *  @since 1.0.1
-     **/
+     */
 
     public void writeLocaleCookie(Locale locale, IEngine engine, RequestContext cycle)
     {
@@ -782,11 +617,75 @@ public class ApplicationServlet extends HttpServlet
      * 
      *  @since 2.3
      *
-     **/
+     */
 
     public ClassResolver getClassResolver()
     {
         return _resolver;
+    }
+
+    /**
+     * Invoked from {@link #init(ServletConfig)} to construct the Registry to
+     * be used by the application.
+     * 
+     * @since 3.1
+     */
+    protected void constructRegistry()
+    {
+        // This will expand in the future.
+
+        _registry = RegistryBuilder.constructDefaultRegistry();
+    }
+
+    /**
+     * Invoked from {@link #init(ServletConfig)}, after the registry has
+     * been constructed, to bootstrap the application via the
+     * <code>tapestry.MasterApplicationInitializer</code> service.
+     */
+    protected void initializeApplication()
+    {
+        ApplicationInitializer ai =
+            (ApplicationInitializer) _registry.getService(
+                "tapestry.MasterInitializer",
+                ApplicationInitializer.class);
+
+        ai.initialize(this);
+
+        _registry.cleanupThread();
+
+        // This is temporary, since most of the code still gets the
+        // specification from the servlet.
+
+        ApplicationGlobals ag =
+            (ApplicationGlobals) _registry.getService(
+                "tapestry.ApplicationGlobals",
+                ApplicationGlobals.class);
+
+        _specification = ag.getSpecification();
+    }
+
+    /**
+     * Returns the Registry used by the application.
+     * 
+     * @since 3.1
+     */
+    public Registry getRegistry()
+    {
+        return _registry;
+    }
+
+    /**
+     * Shuts down the registry (if it exists).
+     * 
+     * @since 3.1
+     */
+    public void destroy()
+    {
+        if (_registry != null)
+        {
+            _registry.shutdown();
+            _registry = null;
+        }
     }
 
 }
