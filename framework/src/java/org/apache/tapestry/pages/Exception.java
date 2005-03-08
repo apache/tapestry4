20b79e09ee16d60070263e4d4b814e9af6f1737f
Howard Lewis Ship <hlship@apache.org>
2005-03-08 22:36:40 +0000

    Move the code for displaying the request objects into its own component, RequestDisplay. Adjust the Workbench's Border component to use the RequestDisplay component and adjust the CSS stylesheet to match.
    git-svn-id: https://svn.eu.apache.org/repos/asf/jakarta/tapestry/trunk@244148 13f79535-47bb-0310-9956-ffa450edef68

--- framework/src/java/org/apache/tapestry/pages/Exception.java
+++ framework/src/java/org/apache/tapestry/pages/Exception.java
@@ -14,21 +14,10 @@
 
 package org.apache.tapestry.pages;
 
-import java.util.Collections;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Properties;
-import java.util.StringTokenizer;
-
-import org.apache.tapestry.IMarkupWriter;
-import org.apache.tapestry.IRender;
-import org.apache.tapestry.IRequestCycle;
-import org.apache.tapestry.describe.RenderableAdapterFactory;
 import org.apache.tapestry.event.PageDetachListener;
 import org.apache.tapestry.html.BasePage;
 import org.apache.tapestry.util.exception.ExceptionAnalyzer;
 import org.apache.tapestry.util.exception.ExceptionDescription;
-import org.apache.tapestry.web.WebUtils;
 
 /**
  * Default exception reporting page.
@@ -49,81 +38,4 @@ public abstract class Exception extends BasePage implements PageDetachListener
 
         setExceptions(exceptions);
     }
-
-    public void renderSystemProperties(IMarkupWriter writer)
-    {
-
-        Properties p = System.getProperties();
-
-        String pathSeparator = p.getProperty("path.separator");
-
-        writer.begin("div");
-        writer.attribute("class", "described-object-title");
-        writer.print("JVM System Properties");
-        writer.end();
-        writer.println();
-
-        writer.begin("table");
-        writer.attribute("class", "described-object");
-
-        Iterator i = WebUtils.toSortedList(p.keys()).iterator();
-
-        while (i.hasNext())
-        {
-            String key = (String) i.next();
-            String value = p.getProperty(key);
-
-            // TODO: Split things that look like paths.
-
-            renderKeyAndValue(writer, key, value, pathSeparator);
-        }
-
-        writer.end();
-    }
-
-    private void renderKeyAndValue(IMarkupWriter writer, String key, String value,
-            String pathSeparator)
-    {
-        String[] values = split(key, value, pathSeparator);
-
-        for (int i = 0; i < values.length; i++)
-        {
-            writer.begin("tr");
-            writer.begin("th");
-
-            if (i == 0)
-                writer.print(key);
-
-            writer.end();
-            writer.begin("td");
-            writer.print(values[i]);
-            writer.end("tr");
-            writer.println();
-        }
-    }
-
-    private String[] split(String key, String value, String pathSeparator)
-    {
-        if (!key.endsWith(".path"))
-            return new String[]
-            { value };
-
-        StringTokenizer tokenizer = new StringTokenizer(value, pathSeparator);
-        List values = Collections.list(tokenizer);
-
-        return (String[]) values.toArray(new String[values.size()]);
-    }
-
-    public IRender getSystemPropertiesRenderer()
-    {
-        return new IRender()
-        {
-            public void render(IMarkupWriter writer, IRequestCycle cycle)
-            {
-                renderSystemProperties(writer);
-            }
-        };
-    }
-
-    public abstract RenderableAdapterFactory getAdapterFactory();
 }
