8206269bbe6f8c6fef8fcf20aade82700d7c05f5
Howard Lewis Ship <hlship@apache.org>
2005-03-10 23:24:33 +0000

    More Portlet code and documentation.
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/tapestry/trunk@244153 13f79535-47bb-0310-9956-ffa450edef68

---
+++ portlet/src/java/org/apache/tapestry/portlet/PortletWebResponse.java
@@ -0,0 +1,69 @@
+// Copyright 2005 The Apache Software Foundation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package org.apache.tapestry.portlet;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+import javax.portlet.PortletResponse;
+
+import org.apache.hivemind.util.Defense;
+import org.apache.tapestry.web.WebResponse;
+
+/**
+ * Adapts {@link javax.portlet.PortletResponse}. as {@link org.apache.tapestry.web.WebResponse}.
+ * 
+ * @author Howard M. Lewis Ship
+ * @since 3.1
+ */
+public class PortletWebResponse implements WebResponse
+{
+    private final PortletResponse _portletResponse;
+
+    public PortletWebResponse(PortletResponse portletResponse)
+    {
+        Defense.notNull(portletResponse, "portletResponse");
+
+        _portletResponse = portletResponse;
+    }
+
+    public OutputStream getOutputStream() throws IOException
+    {
+        unsupported("getOutputStream");
+
+        return null;
+    }
+
+    public String encodeURL(String url)
+    {
+        return _portletResponse.encodeURL(url);
+    }
+
+    public void reset()
+    {
+        unsupported("reset");
+    }
+
+    public void setContentType(String contentType)
+    {
+        unsupported("setContentType");
+    }
+
+    protected final void unsupported(String methodName)
+    {
+        throw new UnsupportedOperationException(PortletMessages.unsupportedMethod(methodName));
+
+    }
+}
---
+++ portlet/src/test/org/apache/tapestry/portlet/TestRenderWebResponse.java
@@ -0,0 +1,87 @@
+// Copyright 2005 The Apache Software Foundation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package org.apache.tapestry.portlet;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+
+import javax.portlet.RenderResponse;
+
+import org.easymock.MockControl;
+
+/**
+ * Tests for {@link org.apache.tapestry.portlet.RenderWebResponse}.
+ * 
+ * @author Howard M. Lewis Ship
+ * @since 3.1
+ */
+public class TestRenderWebResponse extends BasePortletWebTestCase
+{
+    private RenderResponse newResponse()
+    {
+        return (RenderResponse) newMock(RenderResponse.class);
+    }
+
+    public void testReset()
+    {
+        RenderResponse response = newResponse();
+
+        response.reset();
+
+        replayControls();
+
+        RenderWebResponse rwr = new RenderWebResponse(response);
+
+        rwr.reset();
+
+        verifyControls();
+    }
+
+    public void testSetContentType()
+    {
+        RenderResponse response = newResponse();
+
+        response.setContentType("text/html");
+
+        replayControls();
+
+        RenderWebResponse rwr = new RenderWebResponse(response);
+
+        rwr.setContentType("text/html");
+
+        verifyControls();
+    }
+
+    public void testGetOutputStream() throws Exception
+    {
+        MockControl control = newControl(RenderResponse.class);
+        RenderResponse response = (RenderResponse) control.getMock();
+
+        OutputStream stream = new FileOutputStream(File.createTempFile("test", "txt"));
+
+        response.getPortletOutputStream();
+        control.setReturnValue(stream);
+
+        replayControls();
+
+        RenderWebResponse rwr = new RenderWebResponse(response);
+        assertSame(stream, rwr.getOutputStream());
+
+        verifyControls();
+
+        stream.close();
+    }
+}
