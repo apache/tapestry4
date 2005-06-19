01351d597171373f24fff466509ac1796703fb71
Howard Lewis Ship <hlship@apache.org>
2005-06-19 16:06:29 +0000

    Add injection of pages.
    git-svn-id: https://svn.eu.apache.org/repos/asf/jakarta/tapestry/trunk@244332 13f79535-47bb-0310-9956-ffa450edef68

---
+++ framework/src/java/org/apache/tapestry/enhance/InjectPageWorker.java
@@ -0,0 +1,70 @@
+// Copyright 2005 The Apache Software Foundation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package org.apache.tapestry.enhance;
+
+import java.lang.reflect.Modifier;
+
+import org.apache.hivemind.ApplicationRuntimeException;
+import org.apache.hivemind.Location;
+import org.apache.hivemind.service.BodyBuilder;
+import org.apache.hivemind.service.MethodSignature;
+import org.apache.tapestry.IPage;
+import org.apache.tapestry.spec.InjectSpecification;
+
+/**
+ * Injects code to access a named page within the application.
+ * 
+ * @author Howard Lewis Ship
+ * @since 4.0
+ */
+public class InjectPageWorker implements InjectEnhancementWorker
+{
+    public void performEnhancement(EnhancementOperation op, InjectSpecification spec)
+    {
+        performEnhancement(op, spec.getObject(), spec.getProperty(), spec.getLocation());
+    }
+
+    public void performEnhancement(EnhancementOperation op, String pageName, String propertyName,
+            Location location)
+    {
+        Class propertyType = op.getPropertyType(propertyName);
+
+        if (propertyType == null)
+            propertyType = Object.class;
+        else if (propertyType.isPrimitive())
+            throw new ApplicationRuntimeException(EnhanceMessages.wrongTypeForPageInjection(
+                    propertyName,
+                    propertyType), null, location, null);
+
+        op.claimProperty(propertyName);
+
+        MethodSignature sig = new MethodSignature(propertyType, op
+                .getAccessorMethodName(propertyName), null, null);
+
+        BodyBuilder builder = new BodyBuilder();
+
+        builder.add("return ");
+
+        // If the property type is not IPage or a superclass of IPage then a cast
+        // is needed.
+
+        if (!propertyType.isAssignableFrom(IPage.class))
+            builder.add("({0})", propertyType.getName());
+
+        builder.add("getPage().getRequestCycle().getPage(\"{0}\");", pageName);
+
+        op.addMethod(Modifier.PUBLIC, sig, builder.toString());
+    }
+}
---
+++ framework/src/test/org/apache/tapestry/enhance/TestInjectPageWorker.java
@@ -0,0 +1,138 @@
+// Copyright 2005 The Apache Software Foundation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package org.apache.tapestry.enhance;
+
+import java.lang.reflect.Modifier;
+
+import org.apache.hivemind.ApplicationRuntimeException;
+import org.apache.hivemind.Location;
+import org.apache.hivemind.service.MethodSignature;
+import org.apache.hivemind.test.HiveMindTestCase;
+import org.apache.tapestry.html.BasePage;
+import org.apache.tapestry.spec.InjectSpecification;
+import org.apache.tapestry.spec.InjectSpecificationImpl;
+import org.easymock.MockControl;
+
+/**
+ * Tests for {@link org.apache.tapestry.enhance.InjectPageWorker}.
+ * 
+ * @author Howard Lewis Ship
+ * @since 4.0
+ */
+public class TestInjectPageWorker extends HiveMindTestCase
+{
+    public void testPrimitivePropertyType()
+    {
+        Location l = newLocation();
+
+        MockControl opc = newControl(EnhancementOperation.class);
+        EnhancementOperation op = (EnhancementOperation) opc.getMock();
+
+        op.getPropertyType("somePage");
+        opc.setReturnValue(int.class);
+
+        replayControls();
+
+        InjectSpecification is = new InjectSpecificationImpl();
+        is.setProperty("somePage");
+        is.setObject("SomePage");
+        is.setLocation(l);
+
+        try
+        {
+            new InjectPageWorker().performEnhancement(op, is);
+            unreachable();
+        }
+        catch (ApplicationRuntimeException ex)
+        {
+            assertEquals(
+                    "Property somePage is type int, which is not compatible with injection. The property type should be Object, IPage, or a specific page class.",
+                    ex.getMessage());
+            assertSame(l, ex.getLocation());
+        }
+
+        verifyControls();
+    }
+
+    /**
+     * Test for when there's no existing property.
+     */
+
+    public void testNoPropertyType()
+    {
+        MockControl opc = newControl(EnhancementOperation.class);
+        EnhancementOperation op = (EnhancementOperation) opc.getMock();
+
+        op.getPropertyType("somePage");
+        opc.setReturnValue(null);
+
+        op.claimProperty("somePage");
+
+        op.getAccessorMethodName("somePage");
+        opc.setReturnValue("getSomePage");
+
+        MethodSignature sig = new MethodSignature(Object.class, "getSomePage", null, null);
+
+        op.addMethod(
+                Modifier.PUBLIC,
+                sig,
+                "return getPage().getRequestCycle().getPage(\"SomePage\");");
+
+        replayControls();
+
+        InjectSpecification is = new InjectSpecificationImpl();
+        is.setProperty("somePage");
+        is.setObject("SomePage");
+
+        new InjectPageWorker().performEnhancement(op, is);
+
+        verifyControls();
+    }
+
+    public void testExistingPropertyType()
+    {
+        MockControl opc = newControl(EnhancementOperation.class);
+        EnhancementOperation op = (EnhancementOperation) opc.getMock();
+
+        op.getPropertyType("somePage");
+        opc.setReturnValue(BasePage.class);
+
+        op.claimProperty("somePage");
+
+        op.getAccessorMethodName("somePage");
+        opc.setReturnValue("getSomePage");
+
+        MethodSignature sig = new MethodSignature(BasePage.class, "getSomePage", null, null);
+
+        // BasePage is too specific (the getPage() method returns IPage),
+        // so we see a cast.
+
+        op
+                .addMethod(
+                        Modifier.PUBLIC,
+                        sig,
+                        "return (org.apache.tapestry.html.BasePage)getPage().getRequestCycle().getPage(\"SomePage\");");
+
+        replayControls();
+
+        InjectSpecification is = new InjectSpecificationImpl();
+        is.setProperty("somePage");
+        is.setObject("SomePage");
+
+        new InjectPageWorker().performEnhancement(op, is);
+
+        verifyControls();
+    }
+}
