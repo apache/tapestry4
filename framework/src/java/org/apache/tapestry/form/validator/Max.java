d2e843fcf67afccaa8a05f8bc209b3674d679b96
Howard Lewis Ship <hlship@apache.org>
2005-06-20 15:07:52 +0000

    Add Min, Max and MaxDate. Remove DateValidator (replaced by MinDate and MaxDate), NumberValidator (replaced by Min and Max) and StringValidator (replaced by MinLength and MaxLength).
    git-svn-id: https://svn.eu.apache.org/repos/asf/jakarta/tapestry/trunk@244334 13f79535-47bb-0310-9956-ffa450edef68

---
+++ framework/src/java/org/apache/tapestry/form/validator/Max.java
@@ -0,0 +1,105 @@
+// Copyright 2005 The Apache Software Foundation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package org.apache.tapestry.form.validator;
+
+import org.apache.hivemind.util.PropertyUtils;
+import org.apache.tapestry.IMarkupWriter;
+import org.apache.tapestry.IRequestCycle;
+import org.apache.tapestry.form.FormComponentContributorContext;
+import org.apache.tapestry.form.IFormComponent;
+import org.apache.tapestry.form.ValidationMessages;
+import org.apache.tapestry.valid.ValidationConstraint;
+import org.apache.tapestry.valid.ValidationStrings;
+import org.apache.tapestry.valid.ValidatorException;
+
+/**
+ * Validates that the input value is not larger than a particular maximum value.
+ * 
+ * @author Howard Lewis Ship
+ * @since 4.0
+ */
+public class Max implements Validator
+{
+    private double _max;
+
+    private String _message;
+
+    public Max()
+    {
+    }
+
+    public Max(String initializer)
+    {
+        PropertyUtils.configureProperties(this, initializer);
+    }
+
+    /**
+     * Does comparison based on the {@link Number#doubleValue()}.
+     */
+
+    public void validate(IFormComponent field, ValidationMessages messages, Object object)
+            throws ValidatorException
+    {
+        Number value = (Number) object;
+
+        if (value.doubleValue() > _max)
+            throw new ValidatorException(buildMessage(messages, field),
+                    ValidationConstraint.TOO_LARGE);
+    }
+
+    private String buildMessage(ValidationMessages messages, IFormComponent field)
+    {
+        return messages.formatValidationMessage(
+                _message,
+                ValidationStrings.VALUE_TOO_LARGE,
+                new Object[]
+                { field.getDisplayName(), new Double(_max) });
+    }
+
+    /** Returns false. */
+    public boolean getAcceptsNull()
+    {
+        return false;
+    }
+
+    public void renderContribution(IMarkupWriter writer, IRequestCycle cycle,
+            FormComponentContributorContext context, IFormComponent field)
+    {
+        context.includeClasspathScript("/org/apache/tapestry/form/validator/NumberValidator.js");
+
+        String message = buildMessage(context, field);
+
+        StringBuffer buffer = new StringBuffer("function(event) { validate_max_number(event, ");
+        buffer.append(context.getFieldDOM());
+        buffer.append(", ");
+        buffer.append(_max);
+        buffer.append(", '");
+        buffer.append(message);
+        buffer.append("'); }");
+
+        context.addSubmitListener(buffer.toString());
+    }
+
+    public void setMessage(String message)
+    {
+        _message = message;
+    }
+
+    public void setMax(double max)
+    {
+        _max = max;
+    }
+
+}
---
+++ framework/src/test/org/apache/tapestry/form/validator/TestMin.java
@@ -0,0 +1,154 @@
+// Copyright 2005 The Apache Software Foundation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package org.apache.tapestry.form.validator;
+
+import org.apache.tapestry.IMarkupWriter;
+import org.apache.tapestry.IRequestCycle;
+import org.apache.tapestry.form.FormComponentContributorContext;
+import org.apache.tapestry.form.IFormComponent;
+import org.apache.tapestry.form.ValidationMessages;
+import org.apache.tapestry.valid.ValidationConstraint;
+import org.apache.tapestry.valid.ValidationStrings;
+import org.apache.tapestry.valid.ValidatorException;
+import org.easymock.MockControl;
+
+/**
+ * Tests for {@link org.apache.tapestry.form.validator.Min}.
+ * 
+ * @author Howard Lewis Ship
+ * @since 4.0
+ */
+public class TestMin extends BaseValidatorTestCase
+{
+    public void testOK() throws Exception
+    {
+        IFormComponent field = newField();
+        ValidationMessages messages = newMessages();
+
+        Integer object = new Integer(10);
+
+        replayControls();
+
+        new Min("min=5").validate(field, messages, object);
+
+        verifyControls();
+    }
+
+    public void testFail()
+    {
+        IFormComponent field = newField("My Field");
+        ValidationMessages messages = newMessages(
+                null,
+                ValidationStrings.VALUE_TOO_SMALL,
+                new Object[]
+                { "My Field", new Double(10) },
+                "Exception!");
+
+        replayControls();
+
+        try
+        {
+            new Min("min=10").validate(field, messages, new Integer(3));
+        }
+        catch (ValidatorException ex)
+        {
+            assertEquals("Exception!", ex.getMessage());
+            assertEquals(ValidationConstraint.TOO_SMALL, ex.getConstraint());
+        }
+    }
+
+    public void testFailCustomMessage()
+    {
+        IFormComponent field = newField("My Field");
+        ValidationMessages messages = newMessages(
+                "custom",
+                ValidationStrings.VALUE_TOO_SMALL,
+                new Object[]
+                { "My Field", new Double(10) },
+                "custom message");
+
+        replayControls();
+
+        try
+        {
+            new Min("min=10,message=custom").validate(field, messages, new Integer(3));
+        }
+        catch (ValidatorException ex)
+        {
+            assertEquals("custom message", ex.getMessage());
+            assertEquals(ValidationConstraint.TOO_SMALL, ex.getConstraint());
+        }
+    }
+
+    public void testRenderContribution()
+    {
+        IMarkupWriter writer = newWriter();
+        IRequestCycle cycle = newCycle();
+        IFormComponent field = newField("My Field");
+        MockControl contextc = newControl(FormComponentContributorContext.class);
+        FormComponentContributorContext context = (FormComponentContributorContext) contextc
+                .getMock();
+
+        context.includeClasspathScript("/org/apache/tapestry/form/validator/NumberValidator.js");
+
+        trainFormatMessage(contextc, context, null, ValidationStrings.VALUE_TOO_SMALL, new Object[]
+        { "My Field", new Double(20) }, "default message");
+
+        context.getFieldDOM();
+        contextc.setReturnValue("document.myform.myfield");
+
+        context
+                .addSubmitListener("function(event) { validate_min_number(event, document.myform.myfield, 20.0, 'default message'); }");
+
+        replayControls();
+
+        new Min("min=20").renderContribution(writer, cycle, context, field);
+
+        verifyControls();
+    }
+
+    public void testRenderContributionCustomMessage()
+    {
+        IMarkupWriter writer = newWriter();
+        IRequestCycle cycle = newCycle();
+        IFormComponent field = newField("My Field");
+        MockControl contextc = newControl(FormComponentContributorContext.class);
+        FormComponentContributorContext context = (FormComponentContributorContext) contextc
+                .getMock();
+
+        context.includeClasspathScript("/org/apache/tapestry/form/validator/NumberValidator.js");
+
+        trainFormatMessage(
+                contextc,
+                context,
+                "custom",
+                ValidationStrings.VALUE_TOO_SMALL,
+                new Object[]
+                { "My Field", new Double(20) },
+                "custom message");
+
+        context.getFieldDOM();
+        contextc.setReturnValue("document.myform.myfield");
+
+        context
+                .addSubmitListener("function(event) { validate_min_number(event, document.myform.myfield, 20.0, 'custom message'); }");
+
+        replayControls();
+
+        new Min("min=20,message=custom").renderContribution(writer, cycle, context, field);
+
+        verifyControls();
+    }
+}
