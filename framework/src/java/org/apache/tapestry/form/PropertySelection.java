b751e49a7bc78d35dba5562f6095b08c26e98304
Howard Lewis Ship <hlship@apache.org>
2005-08-14 16:15:28 +0000

    TAPESTRY-94: PropertySelection does not submit Palette properly
    git-svn-id: https://svn.eu.apache.org/repos/asf/jakarta/tapestry/trunk@244484 13f79535-47bb-0310-9956-ffa450edef68

--- framework/src/java/org/apache/tapestry/form/PropertySelection.java
+++ framework/src/java/org/apache/tapestry/form/PropertySelection.java
@@ -60,7 +60,7 @@ public abstract class PropertySelection extends AbstractFormComponent implements
             writer.attribute("disabled", "disabled");
 
         if (getSubmitOnChange())
-            writer.attribute("onchange", "this.form.events.submit()");
+            writer.attribute("onchange", "javascript:   this.form.events.submit();");
 
         renderIdAttribute(writer, cycle);
 
