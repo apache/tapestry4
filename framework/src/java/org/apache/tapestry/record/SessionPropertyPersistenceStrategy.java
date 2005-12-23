a569139013a31a210dd7e32fb3c396713b5908bc
Jesse Kuhnert <jkuhnert@apache.org>
2005-12-23 00:37:59 +0000

    I couldn't live with even the two warnings.. :/
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/tapestry/trunk@358670 13f79535-47bb-0310-9956-ffa450edef68

--- framework/src/java/org/apache/tapestry/record/SessionPropertyPersistenceStrategy.java
+++ framework/src/java/org/apache/tapestry/record/SessionPropertyPersistenceStrategy.java
@@ -72,9 +72,9 @@ public class SessionPropertyPersistenceStrategy implements PropertyPersistenceSt
 
         WebSessionAttributeCallback callback = new WebSessionAttributeCallback()
         {
-            public void handleAttribute(WebSession session, String name)
+            public void handleAttribute(WebSession sess, String name)
             {
-                PropertyChange change = RecordUtils.buildChange(name, session.getAttribute(name));
+                PropertyChange change = RecordUtils.buildChange(name, sess.getAttribute(name));
 
                 result.add(change);
             }
@@ -99,9 +99,9 @@ public class SessionPropertyPersistenceStrategy implements PropertyPersistenceSt
 
         WebSessionAttributeCallback callback = new WebSessionAttributeCallback()
         {
-            public void handleAttribute(WebSession session, String name)
+            public void handleAttribute(WebSession sess, String name)
             {
-                session.setAttribute(name, null);
+                sess.setAttribute(name, null);
             }
         };
 
