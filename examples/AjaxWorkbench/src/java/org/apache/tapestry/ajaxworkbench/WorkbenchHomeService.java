5cec410c6c5a1f177a9134886960992f6b86e620
Jesse Kuhnert <jkuhnert@apache.org>
2006-01-24 03:04:45 +0000

    Added new workbench demo space for testing/playing with new 4.1 features
    git-svn-id: https://svn.eu.apache.org/repos/asf/jakarta/tapestry/trunk@371786 13f79535-47bb-0310-9956-ffa450edef68

---
+++ examples/AjaxWorkbench/src/java/org/apache/tapestry/ajaxworkbench/WorkbenchHomeService.java
@@ -0,0 +1,43 @@
+// Copyright 2004, 2005, 2006 The Apache Software Foundation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package org.apache.tapestry.ajaxworkbench;
+
+import java.io.IOException;
+
+import org.apache.tapestry.IRequestCycle;
+import org.apache.tapestry.engine.HomeService;
+
+/**
+ * Special version of the home service used to reset the visit tab when
+ * re-entering the Tapestry application from a static HTML page.
+ * 
+ * @author Howard Lewis Ship
+ * @see Redirect
+ */
+
+public class WorkbenchHomeService extends HomeService
+{
+
+    public void service(IRequestCycle cycle)
+        throws IOException
+    {
+        Visit visit = (Visit)cycle.getEngine().getVisit();
+
+        if (visit != null) visit.setActiveTabName("Home");
+
+        super.service(cycle);
+    }
+
+}
---
+++ examples/AjaxWorkbench/src/java/org/apache/tapestry/ajaxworkbench/fields/Fields.java
@@ -0,0 +1,69 @@
+// Copyright 2004, 2005 The Apache Software Foundation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package org.apache.tapestry.ajaxworkbench.fields;
+
+import java.math.BigDecimal;
+
+import org.apache.tapestry.IPage;
+import org.apache.tapestry.annotations.InjectPage;
+import org.apache.tapestry.html.BasePage;
+import org.apache.tapestry.valid.IValidationDelegate;
+
+/**
+ * @author Howard Lewis Ship
+ * @since 1.0.7
+ */
+
+public abstract class Fields extends BasePage
+{
+    public static final int INT_MIN = 5;
+
+    public static final int INT_MAX = 20;
+
+    public static final double DOUBLE_MIN = 3.14;
+
+    public static final double DOUBLE_MAX = 27.5;
+
+    public static final BigDecimal DECIMAL_MIN = new BigDecimal("2");
+
+    public static final BigDecimal DECIMAL_MAX = new BigDecimal(
+            "100.123456234563456734563456356734567456784567456784567845675678456785678");
+
+    public static final long LONG_MIN = 6;
+
+    public static final long LONG_MAX = 21;
+
+    public static final int STRING_MIN_LENGTH = 3;
+
+    @InjectPage("FieldsResults")
+    public abstract FieldsResults getResultsPage();
+
+    public IPage doSubmit()
+    {
+        return getResultsPage();
+    }
+
+    public void doByLink()
+    {
+        getResultsPage().setByLink(true);
+    }
+
+    /**
+     * Injected.
+     * 
+     * @since 4.0
+     */
+    public abstract IValidationDelegate getDelegate();
+}
