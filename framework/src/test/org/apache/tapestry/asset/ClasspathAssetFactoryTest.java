0c9dc6bf6da01fc102fcd3ad13f47481c98a6be1
Jesse Kuhnert <jkuhnert@apache.org>
2006-01-31 08:20:20 +0000

    still trying to get asset's to play nicely with my non standard includes
    git-svn-id: https://svn.eu.apache.org/repos/asf/jakarta/tapestry/trunk@373758 13f79535-47bb-0310-9956-ffa450edef68

--- framework/src/test/org/apache/tapestry/asset/ClasspathAssetFactoryTest.java
+++ framework/src/test/org/apache/tapestry/asset/ClasspathAssetFactoryTest.java
@@ -182,7 +182,7 @@ public class ClasspathAssetFactoryTest extends HiveMindTestCase
         factory.setAssetService(assetService);
         factory.setLocalizer(new DefaultResourceLocalizer());
         
-        String path = "/org/apache/tapestry/ajax/dojo";
+        String path = "/org/apache/tapestry/ajax/dojo/dojo.js";
         
         IAsset asset = factory.createAsset(shell, path, 
                 Locale.getDefault(),
