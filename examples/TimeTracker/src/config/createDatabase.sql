f8abed493680925ef0970a5ddd231a3dd920fc00
Jesse Kuhnert <jkuhnert@apache.org>
2006-02-11 22:57:45 +0000

    Added a new property selection model class, more timetracker demo work
    git-svn-id: https://svn.eu.apache.org/repos/asf/jakarta/tapestry/trunk@377078 13f79535-47bb-0310-9956-ffa450edef68

--- examples/TimeTracker/src/config/createDatabase.sql
+++ examples/TimeTracker/src/config/createDatabase.sql
@@ -1,8 +1,25 @@
 create table projects (
 	project_id		integer 		not null generated always as identity(start with 100, increment by 1),
-	name			varchar(40)		not null,
+	name			varchar(40)		not null
 );
+
+alter table projects add constraint projects_pk
+primary key(project_id);
+
+alter table projects add constraint name_unique 
+unique (name);
 
 insert into projects(name) values ('Code Zeitgeist');
 insert into projects(name) values ('Zooland Systems');
 insert into projects(name) values ('Weedasher Industries');
+
+create table tasks (
+	task_id			integer			not null generated always as identity(start with 100, increment by 1),
+	project_id		integer			not null,
+	start_dt		timestamp		not null,
+	end_dt			timestamp		not null,
+	descr_txt		varchar(200)	not null
+);
+
+alter table tasks add constraint tasks_fk
+Foreign Key(project_id) references projects (project_id);
---
+++ examples/TimeTracker/src/java/org/apache/tapestry/timetracker/page/TaskEntryPage.java
@@ -0,0 +1,56 @@
+// Copyright 2004, 2005 The Apache Software Foundation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package org.apache.tapestry.timetracker.page;
+
+import java.util.List;
+
+import org.apache.tapestry.annotations.Component;
+import org.apache.tapestry.annotations.InjectObject;
+import org.apache.tapestry.form.BeanPropertySelectionModel;
+import org.apache.tapestry.form.IPropertySelectionModel;
+import org.apache.tapestry.form.PropertySelection;
+import org.apache.tapestry.html.BasePage;
+import org.apache.tapestry.timetracker.dao.ProjectDao;
+import org.apache.tapestry.timetracker.model.Project;
+
+
+/**
+ * Manages entering tasks.
+ *
+ * @author jkuhnert
+ */
+public abstract class TaskEntryPage extends BasePage
+{
+    
+    @Component(type = "PropertySelection", id = "projectChoose",
+            bindings = { "model=projectModel", "value=selectedProject",
+            "displayName=message:choose.project"})
+    public abstract PropertySelection getProjectSelection();
+    
+    @InjectObject("service:timetracker.dao.ProjectDao")
+    public abstract ProjectDao getProjectDao();
+    
+    public abstract Project getSelectedProject();
+    
+    /**
+     * Selection model for projects.
+     * @return
+     */
+    public IPropertySelectionModel getProjectModel()
+    {
+        List<Project> projects = getProjectDao().listProjects();
+        projects.add(0, new Project(-1, "Choose.."));
+        return new BeanPropertySelectionModel(projects, "name");
+    }
+}
