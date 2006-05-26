de887795ac4c0e0a190805ac95ac520e1d6af739
Jesse Kuhnert <jkuhnert@apache.org>
2006-05-26 17:51:18 +0000

    Fixes TAPESTRY-947 patch from james, changed eclipse checkstyle config to be a local project config so people don't have to add the config to the workspace
    git-svn-id: https://svn.apache.org/repos/asf/tapestry/tapestry4/trunk@409706 13f79535-47bb-0310-9956-ffa450edef68

---
+++ framework/src/test/org/apache/tapestry/services/TestDataSqueezer.java
@@ -0,0 +1,33 @@
+// Copyright May 12, 2006 The Apache Software Foundation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package org.apache.tapestry.services;
+
+import org.apache.hivemind.Registry;
+import org.apache.hivemind.test.HiveMindTestCase;
+
+
+/**
+ * 
+ *
+ */
+public class TestDataSqueezer extends HiveMindTestCase {
+
+    public void testPipeline() throws Exception {
+        final Registry reg = buildFrameworkRegistry( "squeezer-pipeline.xml" );
+        final DataSqueezer squeezer = ( DataSqueezer )reg.getService( DataSqueezer.class );
+        final String squeezedValue = squeezer.squeeze( "Hello, World!" );
+        assertTrue( squeezedValue.startsWith( "dummy:" ) );
+        
+    }
+}
