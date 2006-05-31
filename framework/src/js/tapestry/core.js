257991c5641f28c359f413d5dda8dba5f49c0489
Jesse Kuhnert <jkuhnert@apache.org>
2006-05-31 01:25:21 +0000

    refactoring for cleaner code
    git-svn-id: https://svn.eu.apache.org/repos/asf/tapestry/tapestry4/trunk@410415 13f79535-47bb-0310-9956-ffa450edef68

--- framework/src/js/tapestry/core.js
+++ framework/src/js/tapestry/core.js
@@ -48,6 +48,7 @@ tapestry={
 	 */
 	load:function(type, data, http, kwArgs){
 		dojo.log.debug("Response recieved.");
+		dojo.require("tapestry.html");
 		
 		var resp=data.getElementsByTagName("ajax-response");
 		if (!resp || resp.length < 1 || !resp[0].childNodes) { dojo.log.warn("No ajax-response elements recieved."); return; }
@@ -57,86 +58,9 @@ tapestry={
 			var type=elms[i].getAttribute("type");
 			
 			if (type == "exception") {
-				tapestry.presentException(elms[i], kwArgs);
+				tapestry.html.presentException(elms[i], kwArgs);
 				return;
 			}
 		}
-	},
-	
-	presentException:function(node, kwArgs) {
-		var excnode=document.createElement("div");
-		excnode.setAttribute("id", "exceptiondialog");
-		document.body.appendChild(excnode);
-		
-		var contentnode=document.createElement("div");
-		contentnode.innerHTML=this.getContentAsString(node);
-		dojo.html.setClass(contentnode, "exceptionDialog");
-		
-		var navnode=document.createElement("div");
-		navnode.setAttribute("id", "exceptionDialogHandle");
-		dojo.html.setClass(navnode, "exceptionCloseLink");
-		navnode.appendChild(document.createTextNode("Close"));
-		
-		excnode.appendChild(navnode);
-		excnode.appendChild(contentnode);
-		
-		var dialog=dojo.widget.createWidget("Dialog", {widgetId:"exception"}, excnode);
-		
-		dojo.event.connect(navnode, "onclick", dialog, "hide");
-		dojo.event.connect(dialog, "hide", function(e) {
-			dojo.widget.byId("exception").destroy();
-			var node=dojo.byId("exceptiondialog");
-			if (node) dojo.dom.removeNode(node);
-		});
-		
-		dialog.show();
-	},
-	
-	getContentAsString:function(node){
-		if (typeof node.xml != "undefined")
-			return this.getContentAsStringIE(node);
-		else if (typeof XMLSerializer != "undefined" )
-			return this.getContentAsStringMozilla(node);
-		else
-			return this.getContentAsStringGeneric(node);
-	},
-	
-	getContentAsStringIE:function(node){
-		var s="";
-    	for (var i = 0; i < node.childNodes.length; i++)
-        	s += node.childNodes[i].xml;
-    	return s;
-	},
-	
-	getContentAsStringMozilla:function(node){
-		var xmlSerializer = new XMLSerializer();
-	    var s = "";
-	    for (var i = 0; i < node.childNodes.length; i++) {
-	        s += xmlSerializer.serializeToString(node.childNodes[i]);
-	        if (s == "undefined")
-		        return this.getContentAsStringGeneric(node);
-	    }
-	    return s;
-	},
-	
-	getContentAsStringGeneric:function(node){
-		var s="";
-		if (node == null) { return s; }
-		for (var i = 0; i < node.childNodes.length; i++) {
-			switch (node.childNodes[i].nodeType) {
-				case 1: // ELEMENT_NODE
-				case 5: // ENTITY_REFERENCE_NODE
-					s += tacos.getElementAsStringGeneric(node.childNodes[i]);
-					break;
-				case 3: // TEXT_NODE
-				case 2: // ATTRIBUTE_NODE
-				case 4: // CDATA_SECTION_NODE
-					s += node.childNodes[i].nodeValue;
-					break;
-				default:
-					break;
-			}
-		}
-		return s;	
 	}
 }
