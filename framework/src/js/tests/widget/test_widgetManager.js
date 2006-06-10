9ffa49f5eab1af3f30941c13d512d1cb07e85b98
Jesse Kuhnert <jkuhnert@apache.org>
2006-06-10 13:59:28 +0000

    upgraded dojo
    git-svn-id: https://svn.apache.org/repos/asf/tapestry/tapestry4/trunk@413300 13f79535-47bb-0310-9956-ffa450edef68

--- framework/src/js/tests/widget/test_widgetManager.js
+++ framework/src/js/tests/widget/test_widgetManager.js
@@ -12,8 +12,3 @@ function test_syncfailure_widget(){
 	} catch (e) { jum.assertTrue("test2", e instanceof Error); return; }
 	throw new JUMAssertFailure("Previous test should have failed.");
 }
-
-function test_create_widget(){
-	var dp = new dojo.widget.Checkbox();
-	jum.assertFalse("test1", !dp);
-}
