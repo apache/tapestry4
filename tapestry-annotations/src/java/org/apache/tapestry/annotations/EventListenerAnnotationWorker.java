0ae48dd3a9a77e966c63287e281203eb9cc3edd7
Jesse Kuhnert <jkuhnert@apache.org>
2006-07-15 19:40:25 +0000

    Fixes TAPESTRY-1004 and a few documentation bugs
    git-svn-id: https://svn.apache.org/repos/asf/tapestry/tapestry4/trunk@422284 13f79535-47bb-0310-9956-ffa450edef68

--- tapestry-annotations/src/java/org/apache/tapestry/annotations/EventListenerAnnotationWorker.java
+++ tapestry-annotations/src/java/org/apache/tapestry/annotations/EventListenerAnnotationWorker.java
@@ -47,9 +47,6 @@ public class EventListenerAnnotationWorker implements SecondaryAnnotationWorker
     {
         EventListener listener = method.getAnnotation(EventListener.class);
         
-        if (listener == null)
-            return;
-        
         String[] targets = listener.targets();
         String[] elements = listener.elements();
         String formId = listener.submitForm();
