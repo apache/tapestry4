d28b52cb919feedc40784a4598d96cc249d10e19
Jesse Kuhnert <jkuhnert@apache.org>
2006-07-24 20:03:07 +0000

    Moved core.js require call back to end, just in case.
    git-svn-id: https://svn.eu.apache.org/repos/asf/tapestry/tapestry4/trunk@425171 13f79535-47bb-0310-9956-ffa450edef68

--- tapestry-framework/src/js/tapestry/core.js
+++ tapestry-framework/src/js/tapestry/core.js
@@ -9,8 +9,6 @@ dojo.require("dojo.widget.*");
 dojo.require("dojo.widget.Dialog");
 dojo.require("dojo.html");
 
-dojo.require("tapestry.html");
-
 tapestry={
 	
 	version:"4.1", // tapestry script version
@@ -227,4 +225,4 @@ tapestry={
 	}
 }
 
-// dojo.require("tapestry.html");
+dojo.require("tapestry.html");
