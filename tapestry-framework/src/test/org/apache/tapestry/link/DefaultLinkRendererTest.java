2ef0047bff832ff212e387af1f74ce28500f24e0
Andreas Andreou <andyhot@apache.org>
2006-08-01 00:12:07 +0000

    Removing TODOs + a typo
    git-svn-id: https://svn.apache.org/repos/asf/tapestry/tapestry4/trunk@427383 13f79535-47bb-0310-9956-ffa450edef68

--- tapestry-framework/src/test/org/apache/tapestry/link/DefaultLinkRendererTest.java
+++ tapestry-framework/src/test/org/apache/tapestry/link/DefaultLinkRendererTest.java
@@ -97,7 +97,6 @@ public class DefaultLinkRendererTest extends BaseComponentTestCase
         @Override
         protected boolean getHasBody()
         {
-            // TODO Auto-generated method stub
             return _hasBody;
         }
 
