d80eddd1738cada9f9a7c470efd087ecd80743fb
Andreas Andreou <andyhot@apache.org>
2006-08-02 15:28:49 +0000

    TAPESTRY-199: Define component to support all kinds of relations and placeholder for the data
    git-svn-id: https://svn.eu.apache.org/repos/asf/tapestry/tapestry4/trunk@428028 13f79535-47bb-0310-9956-ffa450edef68

---
+++ tapestry-framework/src/java/org/apache/tapestry/html/Relation.java
@@ -0,0 +1,37 @@
+// Copyright Aug 2, 2006 The Apache Software Foundation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package org.apache.tapestry.html;
+
+import org.apache.tapestry.AbstractComponent;
+import org.apache.tapestry.IMarkupWriter;
+import org.apache.tapestry.IRequestCycle;
+
+/**
+ * Works with the {@link Shell} component to define and append a 
+ * relationship between docuements (typically a stylesheet) to 
+ * the HTML response. 
+ * 
+ * @author andyhot
+ */
+public class Relation extends AbstractComponent
+{
+    /** 
+     * {@inheritDoc}
+     */
+    protected void renderComponent(IMarkupWriter writer, IRequestCycle cycle)
+    {
+        // TODO Auto-generated method stub        
+    }
+
+}
