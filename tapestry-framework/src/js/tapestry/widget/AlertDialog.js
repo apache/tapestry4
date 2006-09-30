ed9dbf3faa8b685830552dad5543a9bdbfe91d9d
Jesse Kuhnert <jkuhnert@apache.org>
2006-09-30 04:00:29 +0000

    Enhancements contributed from Greg Woolsey to make the validation dialog a little more extensible/usable/etc.
    git-svn-id: https://svn.eu.apache.org/repos/asf/tapestry/tapestry4/trunk@451536 13f79535-47bb-0310-9956-ffa450edef68

--- tapestry-framework/src/js/tapestry/widget/AlertDialog.js
+++ tapestry-framework/src/js/tapestry/widget/AlertDialog.js
@@ -11,30 +11,61 @@ dojo.widget.defineWidget(
 	dojo.widget.Dialog,
 	{
 		bgColor: "white",
-		bgOpacity: 0.5,
+		bgOpacity: 0.6,
 		okButton:null,
 		messageNode:null,
 		message:"",
+		
+		dialogClass:"alertDialog",
+		contentClass:"alertContent",
+		buttonClass:"alertButton",
 		
 		postCreate: function(args, frag, parentComp) {
 			dojo.widget.Dialog.prototype.postCreate.call(this, args, frag, parentComp);
 			
 			var content=document.createElement("div");
+			dojo.html.setClass(content, this.contentClass);
 			this.containerNode.appendChild(content);
-			dojo.html.addClass(this.containerNode, "alertDialog");
+			dojo.html.addClass(this.containerNode, this.dialogClass);
 			
 			this.messageNode=document.createElement("div");
 			this.messageNode.innerHTML=this.message;
 			content.appendChild(this.messageNode);
 			
-			var buttNode=document.createElement("div");
-			buttNode.appendChild(document.createTextNode("ok"));
+			var buttNode=document.createElement("button");
+			dojo.html.setClass(buttNode, this.buttonClass);
+			buttNode.innerHTML = "Ok";
 			content.appendChild(buttNode);
 			
-			this.show(); // to fix bug in button
-			this.okButton=dojo.widget.createWidget("Button",{}, buttNode);
-			this.tabStart=this.okButton.domNode;
-			dojo.event.connect(this.okButton, "onClick", this, "hide");
+			this.okButton=buttNode;
+			this.tabStart=this.okButton;
+			this.tabEnd=this.okButton;
+			this.show();
+			dojo.event.connect(this.okButton, "onclick", this, "hideDialog");
+			this.okButton.focus();
+			dojo.event.connect(this.wrapper, 'onkeyup', this, 'dialogKeys');
+			dojo.event.connect(document.body, 'onkeyup', this, 'bodyKeys'); 
+		},
+		
+		dialogKeys:function(e) {
+			if (e.keyCode == e.KEY_ESCAPE) {
+				this.hideDialog(e);
+			}
+			// allow default behavior, but don't let the event keep bubbling/propagating
+			if (e.stopPropagation) {
+				e.stopPropagation();
+			} else { 
+				e.cancelBubble = true; 
+			}
+		},
+		
+		bodyKeys:function(e) {
+			if (e.keyCode == e.KEY_ESCAPE) {
+				this.hideDialog(e);
+			} else if ( ! dojo.dom.isDescendantOf(e.target, this.wrapper, true) ) {
+				dojo.event.browser.stopEvent(e);
+				this.tabStart.focus();
+			}
 		},
 		
 		setMessage:function(str){
@@ -42,10 +73,12 @@ dojo.widget.defineWidget(
 		},
 		
 		hideDialog:function(e){
+			dojo.event.disconnect(this.wrapper, 'onkeyup', this, 'dialogKeys');
+			dojo.event.disconnect(document.body, 'onkeyup', this, 'bodyKeys');
 			this.hideModalDialog();
-			this.okButton.destroy();
+			dojo.dom.removeNode(this.okButton);
 			tapestry.widget.AlertDialog.prototype.destroy.call(this);
-			dojo.dom.removeNode(this.bg);
+			dojo.dom.removeNode(this.bg); 
 		}
 	},
 	"html"
