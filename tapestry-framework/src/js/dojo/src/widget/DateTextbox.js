7594820534819a903f9f914d5ccd1c525d320efd
Jesse Kuhnert <jkuhnert@apache.org>
2007-02-06 04:42:25 +0000

    Gone wit yee!
    git-svn-id: https://svn.apache.org/repos/asf/tapestry/tapestry4/trunk@503977 13f79535-47bb-0310-9956-ffa450edef68

--- tapestry-framework/src/js/dojo/src/widget/DateTextbox.js
+++
@@ -1,6 +0,0 @@
-
-dojo.provide("dojo.widget.DateTextbox");dojo.require("dojo.widget.ValidationTextbox");dojo.require("dojo.date.format");dojo.require("dojo.validate.datetime");dojo.widget.defineWidget(
-"dojo.widget.DateTextbox",dojo.widget.ValidationTextbox,{displayFormat: "",formatLength: "short",isValid: function(){return dojo.date.parse(this.textbox.value, {formatLength:this.formatLength, selector:'dateOnly', locale:this.lang, datePattern: this.displayFormat});}}
-);dojo.widget.defineWidget(
-"dojo.widget.TimeTextbox",dojo.widget.ValidationTextbox,{displayFormat: "",formatLength: "short",isValid: function(){return dojo.date.parse(this.textbox.value, {formatLength:this.formatLength, selector:'timeOnly', locale:this.lang, timePattern: this.displayFormat});}}
-);
--- tapestry-framework/src/js/dojo/src/widget/nls/fr/validate.js
+++
@@ -1,2 +0,0 @@
-
-({"rangeMessage":"* Cette valeur est hors limites.","invalidMessage":"* La valeur saisie est incorrecte.","missingMessage":"* Cette valeur est obligatoire."})
