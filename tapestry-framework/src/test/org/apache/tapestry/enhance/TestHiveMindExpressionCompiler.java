718ca736e4d6e6838e5373047f56a8cb961ef4ca
Jesse Kuhnert <jkuhnert@apache.org>
2007-05-05 19:04:39 +0000

    Resolves OGNL-49. Was getting compilation errors from javassist in disabled caching mode because sometimes the hashcodes were equal for class names...Added a global increment counter to force uniqueness.
    git-svn-id: https://svn.apache.org/repos/asf/tapestry/tapestry4/trunk@535558 13f79535-47bb-0310-9956-ffa450edef68

---
+++ tapestry-framework/src/test/org/apache/tapestry/enhance/TestHiveMindExpressionCompiler.java
@@ -0,0 +1,38 @@
+package org.apache.tapestry.enhance;
+
+import ognl.Node;
+import ognl.Ognl;
+import ognl.OgnlContext;
+import org.apache.tapestry.TestBase;
+import org.apache.tapestry.services.impl.HiveMindExpressionCompiler;
+import org.testng.annotations.Test;
+
+/**
+ * Tests functionality of {@link org.apache.tapestry.services.impl.HiveMindExpressionCompiler}.
+ */
+@Test
+public class TestHiveMindExpressionCompiler extends TestBase {
+
+    HiveMindExpressionCompiler _compiler = new HiveMindExpressionCompiler(new ClassFactoryImpl());
+
+    public void test_Duplicate_Class_Compiler()
+    throws Exception
+    {
+        OgnlContext context = (OgnlContext) Ognl.createDefaultContext(null);
+        BasicObject root = new BasicObject();
+        String expr = "name";
+        Node expression = (Node) Ognl.parseExpression(expr);
+
+        _compiler.compileExpression(context, expression, root);
+        assert expression.getAccessor() != null;
+        assertEquals(expression.getAccessor().get(context, root), root.getName());
+
+        Node expression2 = (Node) Ognl.parseExpression(expr);
+        
+        _compiler.compileExpression(context, expression2, root);
+        assert expression2.getAccessor() != null;
+        assertEquals(expression2.getAccessor().get(context, root), root.getName());
+
+        assertNotSame(expression2.getAccessor().getClass().getName(), expression.getAccessor().getClass().getName());
+    }
+}
