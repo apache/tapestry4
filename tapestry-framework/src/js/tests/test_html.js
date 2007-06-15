ec1c0cb4bdaca2a9d8675ed76e9c73af92163e75
Andreas Andreou <andyhot@apache.org>
2007-06-15 04:19:34 +0000

    more regexp fixes and (real-world) test. Input tags aren't expanded but most importantly we stop at the first closing tag - otherwise the matcher gets wicked...
    git-svn-id: https://svn.eu.apache.org/repos/asf/tapestry/tapestry4/trunk@547520 13f79535-47bb-0310-9956-ffa450edef68

--- tapestry-framework/src/js/tests/test_html.js
+++ tapestry-framework/src/js/tests/test_html.js
@@ -32,8 +32,37 @@ function test_html_getElementAsString(){
 }
 
 function test_html_processTextareas(){
-    var initial = "start<textarea id='2' rows=4/>";
-    var expected = "start<textarea id='2' rows=4></textarea>";
+    var initial = "start<textarea id='2' rows=4/>22<input type='text'/><textarea/><div id=1/>";
+    var expected = "start<textarea id='2' rows=4></textarea>22<input type='text'/><textarea></textarea><div id=1></div>";
+    
+    jum.assertEquals(expected, tapestry.html._processCompactElements(initial));
+    jum.assertEquals(expected + expected, 
+        tapestry.html._processCompactElements(initial+initial));
+}
+
+function test_html_processResponse(){
+    var initial = '<div id="editTopic"><form method="post" action="Topics,topicList.$Form.sdirect" id="Form"> '
+        +'<div style="display:none;" id="Formhidden"><input type="hidden" name="formids" value="topicName,shortDescriptiveText,descriptiveText,If,If_0,updateTopic" /> '
+        +'<input type="hidden" name="updateParts" value="list" /> <input type="hidden" name="updateParts" value="edit" /> <input type="hidden" name="reservedids" '
+        +'value="updateParts" /> <input type="hidden" name="submitmode" value="" /> <input type="hidden" name="submitname" value="" /> <input type="hidden" name="If" value="T" /> '
+        +'<input type="hidden" name="If_0" value="F" /> </div> Make the desired changes to this topic. <fieldset> <div> <label for="topicName" class="required">Topic Name</label> '
+        +'<input type="text" name="name" value="Uncategorized" id="tName" /> </div> <div> <label for="shortDescriptiveText" class="required">Short Description</label> '
+        +'<textarea name="shortDescriptiveText" id="shortDescriptiveText" cols="40" rows="4">Information</textarea> </div> <div> <label for="descriptiveText">Additional '
+        +'Description</label> <textarea name="descriptiveText" id="descriptiveText" cols="40" rows="4"/> </div> <div> <label for="pUrlField" class="required">Home '
+        +'Url</label> <input type="text" name="pUrlField" value="ba.org" id="pUrlField" size="40" /> Verified </div> <div></div> </fieldset> <div> '
+        +'<input type="submit" name="uTopic" id="uTopic" value="Save Changes"/> <!--input type="submit" jwcid="cancelTopic@Submit" '
+        +'listener="listener:doCancel" value="message:label.cancel-changes" async="true"/--></div> </form> </div>';
+    var expected = '<div id="editTopic"><form method="post" action="Topics,topicList.$Form.sdirect" id="Form"> '
+        +'<div style="display:none;" id="Formhidden"><input type="hidden" name="formids" value="topicName,shortDescriptiveText,descriptiveText,If,If_0,updateTopic" /> '
+        +'<input type="hidden" name="updateParts" value="list" /> <input type="hidden" name="updateParts" value="edit" /> <input type="hidden" name="reservedids" '
+        +'value="updateParts" /> <input type="hidden" name="submitmode" value="" /> <input type="hidden" name="submitname" value="" /> <input type="hidden" name="If" value="T" /> '
+        +'<input type="hidden" name="If_0" value="F" /> </div> Make the desired changes to this topic. <fieldset> <div> <label for="topicName" class="required">Topic Name</label> '
+        +'<input type="text" name="name" value="Uncategorized" id="tName" /> </div> <div> <label for="shortDescriptiveText" class="required">Short Description</label> '
+        +'<textarea name="shortDescriptiveText" id="shortDescriptiveText" cols="40" rows="4">Information</textarea> </div> <div> <label for="descriptiveText">Additional '
+        +'Description</label> <textarea name="descriptiveText" id="descriptiveText" cols="40" rows="4"></textarea> </div> <div> <label for="pUrlField" class="required">Home '
+        +'Url</label> <input type="text" name="pUrlField" value="ba.org" id="pUrlField" size="40" /> Verified </div> <div></div> </fieldset> <div> '
+        +'<input type="submit" name="uTopic" id="uTopic" value="Save Changes"/> <!--input type="submit" jwcid="cancelTopic@Submit" '
+        +'listener="listener:doCancel" value="message:label.cancel-changes" async="true"/--></div> </form> </div>';
     
     jum.assertEquals(expected, tapestry.html._processCompactElements(initial));
     jum.assertEquals(expected + expected, 
