bb8b8683782d52d2d7e43927d5a03981470212e3
Andreas Andreou <andyhot@apache.org>
2007-09-28 11:41:46 +0000

    TAPESTRY-1783: remove deprecated js
    git-svn-id: https://svn.eu.apache.org/repos/asf/tapestry/tapestry4/trunk@580316 13f79535-47bb-0310-9956-ffa450edef68

--- tapestry-framework/src/js/tests/test_form.js
+++ tapestry-framework/src/js/tests/test_form.js
@@ -7,76 +7,8 @@ dojo.require("tapestry.form");
 tapestry.form.invalidField=function(field, message){
 }
 
-function test_form_find(){
-	var node = document.createElement("div");
-	node.setAttribute("id", "testid");
-
-	jum.assertTrue(Tapestry.find(node));
-	jum.assertTrue(Tapestry.find("testid"));
-}
-
 function test_last_msg(){
 	jum.assertFalse(lastMsgContains());
-}
-
-function test_form_deprecated(){
-	try {
-		Tapestry.register_form();
-		throw new JUMAssertFailure("Previous test should have failed.");
-	} catch (e) { jum.assertTrue("testFormDepre", e instanceof Error); return; }
-	
-	jum.assertTrue("deprecated", lastMsgContains("deprecated"));
-	Tapestry.onpresubmit();
-	jum.assertTrue("deprecated", lastMsgContains("deprecated"));
-	Tapestry.onsubmit();
-	jum.assertTrue("deprecated", lastMsgContains("deprecated"));
-	Tapestry.onpostsubmit();
-	jum.assertTrue("deprecated", lastMsgContains("deprecated"));
-	Tapestry.onreset();
-	jum.assertTrue("deprecated", lastMsgContains("deprecated"));
-	Tapestry.onrefresh();
-	jum.assertTrue("deprecated", lastMsgContains("deprecated"));
-	Tapestry.oncancel();
-	jum.assertTrue("deprecated", lastMsgContains("deprecated"));
-}
-
-function test_form_invalidHandler(){
-	Tapestry.default_invalid_field_handler(null, "yo", "yo");
-}
-
-function test_form_requireTextField(){
-	Tapestry.require_field(null, "bs", "invalid");
-
-	var node = document.createElement("input");
-	node.setAttribute("id", "testid");
-	node.type="text";
-	node.value="";
-
-	var mockInvalid=new mock(node, "must have value");
-	dojo.event.connect(Tapestry, "default_invalid_field_handler", mockInvalid, "intercept");
-
-	Tapestry.require_field(null, "testid", "must have value");
-
-	jum.assertTrue("invalidCalled", mockInvalid.called);
-	dojo.event.disconnect(Tapestry, "default_invalid_field_handler", mockInvalid, "intercept");
-}
-
-function test_form_submit(){
-
-	var submitCalled=false;
-	var node = document.createElement("form");
-	node.setAttribute("id", "form1");
-	node.submit=function(){
-		submitCalled=true;
-	}
-	node.submitname={value:""};
-	node.elements=[];
-
-	Tapestry.register_form("form1");
-	Tapestry.submit_form("form1", "testSubmit");
-
-	jum.assertTrue("submitCalled", submitCalled);
-	jum.assertEquals("submitName", node.submitname.value, "testSubmit");
 }
 
 var bindCalled=false;
