085fb2e47749c6f759fc57e5c49728b6f9543a51
Andreas Andreou <andyhot@apache.org>
2008-04-09 00:01:38 +0000

    update dojo tapestry profile
    git-svn-id: https://svn.eu.apache.org/repos/asf/tapestry/tapestry4/trunk@646148 13f79535-47bb-0310-9956-ffa450edef68

--- tapestry-framework/src/js/tapestry.profile.js
+++ tapestry-framework/src/js/tapestry.profile.js
@@ -3,16 +3,59 @@
 // hostenv files are included by default and don't need to be included here,
 // but you can change the hostenv file that's included by setting the value of
 // the variable "hostenvType" (defaults to "browser").
-var dependencies = [ 
+var dependencies = [
 	"dojo.event.browser",
-    "dojo.dom",
-    "dojo.lang.common",
     "dojo.io.common",
     "dojo.io.BrowserIO",
     "dojo.io.cookie",
-    "dojo.date.common",
-    "dojo.date.format",
-	"dojo.widget.*"
+    "dojo.lang.common",
+    "dojo.lang.declare",
+    "dojo.html.*",
+    "dojo.html.display",
+    "dojo.ns",
+    "dojo.widget.Manager",
+    "dojo.tlocale"
+];
+
+dependencies.layers = [
+	{
+		name: "dojo2.js",
+		resourceName: "layer.validation",
+		layerDependencies: [
+			"dojo.js"
+		],
+		dependencies: [
+            "dojo.i18n.number",
+            "dojo.validate.check",
+            "dojo.string.extras",
+            "dojo.html.style",
+            "dojo.regexp",
+            "dojo.date.format",
+            "dojo.date.serialize",
+            "dojo.validate.datetime",
+            "dojo.validate.web",
+            "dojo.validate.creditCard",
+            "dojo.validate.us"
+        ]
+	},
+	{
+		name: "dojo3.js",
+		resourceName: "layer.widget",
+		layerDependencies: [
+			"dojo2.js"
+		],
+		dependencies: [
+            "dojo.namespaces.dojo",
+            "dojo.widget.*",
+            "dojo.widget.ContentPane",
+            "dojo.widget.PopupContainer",
+            "dojo.widget.DropdownContainer",
+            "dojo.widget.Manager",
+            "dojo.widget.html.stabile",
+            "dojo.widget.Dialog",
+            "dojo.widget.Select"
+        ]
+	}
 ];
 
 // NOTE: this MUST be included or a list of files must be output via print()
